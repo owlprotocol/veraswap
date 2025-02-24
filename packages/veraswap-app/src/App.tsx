8f63181bd7a18191e75b9a113996ad2e4e94fcac
Leo Vigna <leo.vigna@gmail.com>
2025-02-24 13:26:00 -0700

    update veraswap app

--- packages/veraswap-app/src/App.tsx
+++ packages/veraswap-app/src/App.tsx
@@ -1,7 +1,5 @@
 import { RouterProvider } from "@tanstack/react-router";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { API_TRPC_BASE_URL, isProduction } from "@owlprotocol/envvars/browser";
-import { OwlProvider } from "@owlprotocol/ui-components";
 import { ThemeProvider } from "./components/theme-provider.js";
 import { Analytics } from "./components/analytics.js";
 import { router } from "./router.js";
@@ -18,16 +16,8 @@ function App() {
         <>
             {/* Keep CSS out of the App as intended */}
             <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-                <OwlProvider
-                    useAuthIFrame={useAuthIFrame}
-                    apiTrpcBaseUrl={API_TRPC_BASE_URL}
-                >
-                    <InnerApp />
-                    {!isProduction() && (
-                        <ReactQueryDevtools initialIsOpen={false} />
-                    )}
-                </OwlProvider>
-
+                <InnerApp />
+                <ReactQueryDevtools initialIsOpen={false} />
                 <Analytics />
                 <Toaster />
             </ThemeProvider>
