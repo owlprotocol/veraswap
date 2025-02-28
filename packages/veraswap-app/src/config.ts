8804e0072ab1ea4d36968e7121ac0953d9540a3e
Oscar Baracos <oscar.baracos@gmail.com>
2025-02-28 11:00:29 -0700

    Set up wallet connect

--- packages/veraswap-app/src/config.ts
+++ packages/veraswap-app/src/config.ts
@@ -1,26 +1,23 @@
 import { http, createStorage, createConfig } from "wagmi";
-import { Chain, localhost } from "wagmi/chains";
+import { Chain } from "wagmi/chains";
 import { connectorsForWallets } from "@rainbow-me/rainbowkit";
-import { metaMaskWallet } from "@rainbow-me/rainbowkit/wallets";
+import { coinbaseWallet, metaMaskWallet, walletConnectWallet, uniswapWallet } from "@rainbow-me/rainbowkit/wallets";
 import { chains } from "./atoms/index.js";
 
 export const connectors = connectorsForWallets(
     [
         {
             groupName: "Recommended",
-            wallets: [metaMaskWallet],
+            wallets: [metaMaskWallet, coinbaseWallet, walletConnectWallet, uniswapWallet],
         },
     ],
-    { projectId: "veraswap", appName: "Owl Protocol" },
+    { projectId: "c2ad5c78be369f29fba3daa1799f2028", appName: "VeraSwap" },
 );
 
 export const config = createConfig({
     chains,
     // @ts-ignore
     transports: Object.fromEntries(chains.map((chain: Chain) => [chain.id, http()])),
-    // transports: {
-    //   [localhost.id]: http(),
-    // },
     connectors,
     storage: createStorage({ storage: window.localStorage }),
 });
