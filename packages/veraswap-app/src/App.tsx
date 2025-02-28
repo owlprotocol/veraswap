04b32355070f86f39117fb91fe3230332b1f2b27
Oscar Baracos <oscar.baracos@gmail.com>
2025-02-28 12:27:37 -0700

    Fix version mismatches

--- packages/veraswap-app/src/App.tsx
+++ packages/veraswap-app/src/App.tsx
@@ -34,7 +34,6 @@ function App() {
                     <OnchainKitProvider
                         apiKey={import.meta.env.VITE_PUBLIC_ONCHAINKIT_API_KEY}
                         projectId={import.meta.env.NEXT_PUBLIC_CDP_PROJECT_ID}
-                        // @ts-ignore
                         chain={base}
                     >
                         <QueryClientProvider client={queryClient}>
