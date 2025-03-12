4dbae45052a16c569fdcec9abc88598d6ca06671
Leo Vigna <leo.vigna@gmail.com>
2025-03-12 03:49:49 -0700

    add ERC7579Router, OwnableExecutor import

---
+++ packages/veraswap-sdk/contracts/hyperlane/ERC7579Router.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.24;
+
+import {OwnableSignatureExecutor} from "@rhinestone/core-modules/OwnableSignatureExecutor/OwnableSignatureExecutor.sol";
+
+contract X is OwnableSignatureExecutor {}
