6ae5946aa482408e4b7b83dfd8674ef57fe28c28
Leo Vigna <leo.vigna@gmail.com>
2025-03-14 18:29:54 -0300

    update remappings, exports

--- packages/veraswap-sdk/contracts/Exports.sol
+++ packages/veraswap-sdk/contracts/Exports.sol
@@ -2,3 +2,5 @@
 pragma solidity ^0.8.24;
 
 import {IL2ToL2CrossDomainMessenger} from "@interop-lib/interfaces/IL2ToL2CrossDomainMessenger.sol";
+import {Kernel} from "@zerodev/kernel/Kernel.sol";
+import {KernelFactory} from "@zerodev/kernel/factory/KernelFactory.sol";
