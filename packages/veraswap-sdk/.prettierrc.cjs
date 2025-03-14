d8747d5ad3efa2ac345be040e9b5022e28ea72d6
Oscar Baracos <oscar.baracos@gmail.com>
2025-03-14 13:16:35 +0100

    Update TS to 5.8, change everything accordingly

--- packages/veraswap-sdk/.prettierrc.cjs
+++ packages/veraswap-sdk/.prettierrc.cjs
@@ -1,3 +1,15 @@
 module.exports = {
-    ...require("@owlprotocol/prettier-config"),
+    semi: true,
+    trailingComma: 'all',
+    singleQuote: false,
+    printWidth: 120,
+    tabWidth: 4,
+    overrides: [
+        {
+            files: "*.json",
+            options: {
+                tabWidth: 2,
+            }
+        }
+    ]
 };
