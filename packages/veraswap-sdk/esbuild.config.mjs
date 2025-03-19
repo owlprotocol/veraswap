994898ebcfec92e78944c671d9e8d88d1545a82c
Oscar Baracos <oscar.baracos@gmail.com>
2025-03-19 13:05:31 +0100

    Ignore tests in esbuild

--- packages/veraswap-sdk/esbuild.config.mjs
+++ packages/veraswap-sdk/esbuild.config.mjs
@@ -1,7 +1,10 @@
 import { buildLib, cjsLibConfig, esmLibConfig } from "@owlprotocol/esbuild-config";
+import ignoretestsPlugin from "esbuild-plugin-ignoretests";
 
 // Conflicts with forge libraries
 cjsLibConfig.outdir = "_cjs/lib";
 esmLibConfig.outdir = "_esm/lib";
+cjsLibConfig.plugins = [ignoretestsPlugin(), ...cjsLibConfig.plugins];
+esmLibConfig.plugins = [ignoretestsPlugin(), ...esmLibConfig.plugins];
 
-await buildLib();
+await buildLib({});
