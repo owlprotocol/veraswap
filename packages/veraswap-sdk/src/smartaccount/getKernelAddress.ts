766b59fb555bb5e53ae47f1c1b5e71533e4ad24b
Leo Vigna <leo.vigna@gmail.com>
2025-03-20 17:27:52 -0300

    kernel deploy wallet with executor

---
+++ packages/veraswap-sdk/src/smartaccount/getKernelAddress.ts
@@ -0,0 +1,18 @@
+import { Address, encodePacked, Hash, Hex, keccak256 } from "viem";
+import { predictDeterministicAddressERC1967 } from "./LibClone.js";
+
+export interface GetKernelAddressParams {
+    data: Hex;
+    salt: Hash;
+    implementation: Address;
+    factoryAddress: Address;
+}
+/**
+ * Get Kernel v3.1 Address
+ * @params Kernel init params
+ * @return Kernel address
+ */
+export function getKernelAddress({ data, salt, implementation, factoryAddress }: GetKernelAddressParams): Address {
+    const actualSalt = keccak256(encodePacked(["bytes", "bytes32"], [data, salt]));
+    return predictDeterministicAddressERC1967(implementation, actualSalt, factoryAddress);
+}
--- packages/veraswap-sdk/src/smartaccount/index.test.ts
+++ packages/veraswap-sdk/src/smartaccount/index.test.ts
@@ -1,22 +1,27 @@
-import { describe, test } from "vitest";
-import { Account } from "viem";
+import { describe, expect, test } from "vitest";
+import { Account, createWalletClient, encodeFunctionData, http, zeroHash } from "viem";
 import { entryPoint07Address } from "viem/account-abstraction";
 
 import { getAnvilAccount } from "@veraswap/anvil-account";
 
 import { signerToEcdsaValidator } from "@zerodev/ecdsa-validator";
 import { KERNEL_V3_1 } from "@zerodev/sdk/constants";
+import { toKernelPluginManager } from "@zerodev/sdk/accounts";
 
 import { LOCAL_KERNEL_CONTRACTS } from "../constants/kernel.js";
 import { opChainL1Client } from "../constants/chains.js";
 import { getKernelInitData } from "./getKernelInitData.js";
 import { installOwnableExecutor } from "./installOwnableExecutor.js";
-import { toKernelPluginManager } from "@zerodev/sdk/accounts";
+import { KernelFactory } from "../artifacts/KernelFactory.js";
+import { getKernelAddress } from "./getKernelAddress.js";
 
 describe("smartaccount/index.test.ts", function () {
     // Test ZeroDev SDK & Custom SDK Tooling
     const entryPoint = { address: entryPoint07Address, version: "0.7" } as const;
     const kernelVersion = KERNEL_V3_1;
+
+    const account: Account = getAnvilAccount(1);
+    const walletClient = createWalletClient({ account, chain: opChainL1Client.chain, transport: http() });
 
     /**
      * TODO
@@ -30,12 +35,10 @@ describe("smartaccount/index.test.ts", function () {
 
     describe("OwnableExecutor", () => {
         test("installOwnableExecutor", async () => {
-            const signer: Account = getAnvilAccount(1);
-
             const ecdsaValidator = await signerToEcdsaValidator(opChainL1Client, {
                 entryPoint,
                 kernelVersion,
-                signer,
+                signer: account,
                 validatorAddress: LOCAL_KERNEL_CONTRACTS.ecdsaValidator,
             });
             const kernelPluginManager = await toKernelPluginManager<"0.7">(opChainL1Client, {
@@ -44,18 +47,35 @@ describe("smartaccount/index.test.ts", function () {
                 sudo: ecdsaValidator,
                 chainId: opChainL1Client.chain.id,
             });
+            // Kernel `initialize` call
             const initData = await getKernelInitData({
                 kernelPluginManager,
                 initHook: false,
                 initConfig: [
                     installOwnableExecutor({
-                        owner: signer.address,
+                        owner: account.address,
                         executor: LOCAL_KERNEL_CONTRACTS.ownableSignatureExecutor,
                     }),
                 ],
             });
+            // KernelFactory `createAccount` call
+            const kernalAddressPredicted = getKernelAddress({
+                data: initData,
+                salt: zeroHash,
+                implementation: LOCAL_KERNEL_CONTRACTS.kernel,
+                factoryAddress: LOCAL_KERNEL_CONTRACTS.kernelFactory,
+            });
 
-            console.debug({ initData });
+            const hash = await walletClient.writeContract({
+                address: LOCAL_KERNEL_CONTRACTS.kernelFactory,
+                abi: KernelFactory.abi,
+                functionName: "createAccount",
+                args: [initData, zeroHash],
+            });
+            await opChainL1Client.waitForTransactionReceipt({ hash });
+
+            const bytecode = await opChainL1Client.getCode({ address: kernalAddressPredicted });
+            expect(bytecode).toBeDefined();
         });
 
         test("OwnableExecutor.executeOnOwnedAccount - direct", () => { });
