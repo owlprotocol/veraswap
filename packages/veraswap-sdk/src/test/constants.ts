22f0afae7b73b872275bfc0d0f7c62031d0d25ee
Leo Vigna <leo.vigna@gmail.com>
2025-03-20 12:18:29 -0300

    setup vitest, add opChain configs

--- packages/veraswap-sdk/src/test/constants.ts
+++ packages/veraswap-sdk/src/test/constants.ts
@@ -12,3 +12,29 @@ export const localhost2 = {
     name: "Localhost 2",
     rpcUrls: { default: { http: [`http://127.0.0.1:${port2}`] } },
 } as Chain;
+
+/*** Supersim Constants ***/
+// Designed to mimic default supersim config
+export const opChainL1Port = 8545;
+export const opChainL1 = {
+    ...localhost,
+    id: 900,
+    name: "Local",
+    rpcUrls: { default: { http: [`http://127.0.0.1:${opChainL1Port}`] } },
+};
+
+export const opChainAPort = 9545;
+export const opChainA = {
+    ...localhost,
+    id: 901,
+    name: "OPChainA",
+    rpcUrls: { default: { http: [`http://127.0.0.1:${opChainAPort}`] } },
+};
+
+export const opChainBPort = 9546;
+export const opChainB = {
+    ...localhost,
+    id: 902,
+    name: "OPChainB",
+    rpcUrls: { default: { http: [`http://127.0.0.1:${opChainBPort}`] } },
+};
--- packages/veraswap-sdk/vitest.setup.ts
+++ packages/veraswap-sdk/vitest.setup.ts
@@ -2,90 +2,54 @@
 import "core-js/actual/promise";
 import { Instance } from "prool";
 import { anvil } from "prool/instances";
-import { createWalletClient, http } from "viem";
-import { localhost } from "viem/chains";
-// import { setupTestMailboxContracts } from "@owlprotocol/contracts-hyperlane";
-import { getAnvilAccount } from "@veraswap/anvil-account";
 import { promisify } from "node:util";
 import { exec } from "node:child_process";
 
-import { chainId2, localhost2, port, port2 } from "./src/test/constants.js";
+import { opChainL1, opChainL1Port, opChainA, opChainAPort, opChainB, opChainBPort } from "./src/test/constants.js";
 
 const execPromise = promisify(exec);
 
-let instance: Instance;
-let instance2: Instance;
+let chainL1Instance: Instance;
+let chainAInstance: Instance;
+let chainBInstance: Instance;
 
+//TODO: Consider using https://github.com/wevm/prool/pull/29, for now just deploy same infra
 /**
  * Run once on `vitest` command. NOT on test re-runs
  */
 export async function setup() {
     const host = "127.0.0.1";
-    const codeSizeLimit = 393216; //10x normal size
-    instance = anvil({
+    chainL1Instance = anvil({
         host,
-        port,
-        chainId: localhost.id,
-        codeSizeLimit,
+        port: opChainL1Port,
+        chainId: opChainL1.id,
     });
-
-    instance2 = anvil({
+    chainAInstance = anvil({
         host,
-        port: port2,
-        chainId: chainId2,
-        codeSizeLimit,
+        port: opChainAPort,
+        chainId: opChainA.id,
+    });
+    chainBInstance = anvil({
+        host,
+        port: opChainBPort,
+        chainId: opChainB.id,
     });
 
-    await instance.start();
-    await instance2.start();
+    await chainL1Instance.start();
+    await chainAInstance.start();
+    await chainBInstance.start();
 
-    const transport = http(`http://${host}:${port}`);
-    const transport2 = http(`http://${host}:${port2}`);
-
-    // Deploy Deterministic Deployer
-    const walletClient = createWalletClient({
-        account: getAnvilAccount(0),
-        chain: localhost,
-        transport,
-    });
-
-    const walletClient2 = createWalletClient({
-        account: getAnvilAccount(0),
-        chain: localhost2,
-        transport: transport2,
-    });
-
-    // Forge scripts
-    // const privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"; // anvil 0
-    // const templateCommand = `forge script ./test/DeployAnvil.s.sol --rpc-url http://${host}:${port} --private-key ${privateKey} --broadcast --via-ir --code-size-limit ${codeSizeLimit}`;
-    // const { stdout } = await execPromise(templateCommand);
-    // const templateCommand2 = `forge script ./test/DeployAnvil.s.sol --rpc-url http://${host}:${port2} --private-key ${privateKey} --broadcast --via-ir --code-size-limit ${codeSizeLimit}`;
-    // await execPromise(templateCommand2);
-    //
-    // const poolManager = stdout.match(/v4PoolManager: (0x.*?)\n/)?.[1];
-    // const positionManager = stdout.match(/v4PositionManager: (0x.*?)\n/)?.[1];
-    // const universalRouter = stdout.match(/router: (0x.*?)\n/)?.[1];
-    // const quoter = stdout.match(/v4Quoter: (0x.*?)\n/)?.[1];
-    // const stateView = stdout.match(/stateView: (0x.*?)\n/)?.[1];
-    // const mockA = stdout.match(/MockA: (0x.*?)\n/)?.[1];
-    // const mockB = stdout.match(/MockB: (0x.*?)\n/)?.[1];
-    // console.log({ poolManager, positionManager, universalRouter, quoter, stateView, mockA, mockB });
-
-    /*
-    const mailboxContracts = await setupTestMailboxContracts(walletClient);
-    const mailboxAddress = mailboxContracts.mailbox.address;
-
-    const mailboxContracts2 = await setupTestMailboxContracts(walletClient2);
-    const mailboxAddress2 = mailboxContracts2.mailbox.address;
-
-    console.log({ mailboxAddress, mailboxAddress2 });
-    */
+    // Forge script
+    const privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"; // anvil 0
+    const templateCommand = `forge script ./script/DeployAll.s.sol --private-key ${privateKey} --broadcast`;
+    const { stdout } = await execPromise(templateCommand);
 }
 
 /**
  * Run once `vitest` process has exited. NOT on test re-runs
  */
 export async function teardown() {
-    await instance.stop();
-    await instance2.stop();
+    await chainL1Instance.stop();
+    await chainAInstance.stop();
+    await chainBInstance.stop();
 }
