fcc89d526a9088a6b99ade181667d8dab0cd8c15
Leo Vigna <leo.vigna@gmail.com>
2025-03-24 16:19:43 -0300

    add interop chains, hyperlane constants

--- packages/veraswap-sdk/src/constants/chains.ts
+++ packages/veraswap-sdk/src/constants/chains.ts
@@ -1,8 +1,7 @@
 import { Chain, localhost } from "viem/chains";
-import { createPublicClient, http } from "viem";
+import { createPublicClient, defineChain, http } from "viem";
 
-/*** Supersim Constants ***/
-// Designed to mimic default supersim config, can still be used with prool/anvil (if no need for Interop predeploys)
+/*** Local Supersim Chains ***/
 export const opChainL1Port = 8547;
 export const opChainL1 = {
     ...localhost,
@@ -29,3 +28,62 @@ export const opChainB = {
     rpcUrls: { default: { http: [`http://127.0.0.1:${opChainBPort}`] } },
 } as const satisfies Chain;
 export const opChainBClient = createPublicClient({ chain: opChainB, transport: http() });
+
+/*** Devnet chains with Sepolia L1 ***/
+export const interopDevnet0 = defineChain({
+    id: 420120000,
+    name: "Interop Devnet 0",
+    network: "interop-alpha-0",
+    nativeCurrency: {
+        name: "Ether",
+        symbol: "ETH",
+        decimals: 18,
+    },
+    rpcUrls: {
+        default: {
+            http: ["https://interop-alpha-0.optimism.io"],
+        },
+        public: {
+            http: ["https://interop-alpha-0.optimism.io"],
+        },
+    },
+    blockExplorers: {
+        default: { name: "Blockscout", url: "https://optimism-interop-alpha-0.blockscout.com/" },
+        routescan: { name: "RouteScan", url: "https://420120000.testnet.routescan.io/" },
+    },
+    contracts: {
+        OptimismPortal: {
+            address: "0x7385d89d38ab79984e7c84fab9ce5e6f4815468a",
+        },
+    },
+    testnet: true,
+});
+
+export const interopDevnet1 = defineChain({
+    id: 420120001,
+    name: "Interop Devnet 1",
+    network: "interop-alpha-1",
+    nativeCurrency: {
+        name: "Ether",
+        symbol: "ETH",
+        decimals: 18,
+    },
+    rpcUrls: {
+        default: {
+            http: ["https://interop-alpha-1.optimism.io"],
+        },
+        public: {
+            http: ["https://interop-alpha-1.optimism.io"],
+        },
+    },
+    blockExplorers: {
+        default: { name: "Blockscout", url: "https://optimism-interop-alpha-1.blockscout.com/" },
+        routescan: { name: "RouteScan", url: "https://420120001.testnet.routescan.io/" },
+    },
+    contracts: {
+        OptimismPortal: {
+            address: "0x55f5c4653dbcde7d1254f9c690a5d761b315500c",
+        },
+    },
+    testnet: true,
+});
