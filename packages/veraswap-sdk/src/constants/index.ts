ad490129e8aff2171defa767c16696147bb12f1e
Leo Vigna <leo.vigna@gmail.com>
2025-03-24 23:53:14 -0300

    add uint constants

--- packages/veraswap-sdk/src/constants/index.ts
+++ packages/veraswap-sdk/src/constants/index.ts
@@ -2,4 +2,5 @@ export * from "./chains.js";
 export * from "./hyperlane.js";
 export * from "./kernel.js";
 export * from "./tokens.js";
+export * from "./uint256.js";
 export * from "./uniswap.js";
