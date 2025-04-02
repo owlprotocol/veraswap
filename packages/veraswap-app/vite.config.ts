0b70db7d064498849d70932444ccc3399b5cf0c7
Leo Vigna <leo.vigna@gmail.com>
2025-04-02 15:54:14 -0300

    update viteconfig, add node-polyfills, monkeypatch fix

--- packages/veraswap-app/vite.config.ts
+++ packages/veraswap-app/vite.config.ts
@@ -4,11 +4,27 @@ import ReactPlugin from "@vitejs/plugin-react-swc";
 import CheckerPlugin from "vite-plugin-checker";
 import SVGRPlugin from "vite-plugin-svgr";
 import DTSPlugin from "vite-plugin-dts";
+import { nodePolyfills, PolyfillOptions } from "vite-plugin-node-polyfills";
 import path from "path";
+
+// Plugin issue v0.17+ https://github.com/davidmyersdev/vite-plugin-node-polyfills/issues/81#issuecomment-2325104572
+const nodePolyfillsFix = (options?: PolyfillOptions | undefined): Plugin => {
+    return {
+        ...nodePolyfills(options),
+        //@ts-ignore
+        resolveId(source: string) {
+            const m = /^vite-plugin-node-polyfills\/shims\/(buffer|global|process)$/.exec(source);
+            if (m) {
+                return `node_modules/vite-plugin-node-polyfills/shims/${m[1]}/dist/index.cjs`;
+            }
+        },
+    };
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
+        nodePolyfillsFix(),
         ReactPlugin(),
         TanStackRouterVite(),
         SVGRPlugin({
