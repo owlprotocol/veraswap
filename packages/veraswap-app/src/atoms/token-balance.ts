3367d187e75371bdf699dde48bc19cad3ac9feea
Kevin El Sayed <124922925+tota79@users.noreply.github.com>
2025-04-08 11:28:29 -0300

    fix token balances (#129)

--- packages/veraswap-app/src/atoms/token-balance.ts
+++ packages/veraswap-app/src/atoms/token-balance.ts
@@ -2,38 +2,43 @@ import { balanceOf as balanceOfAbi } from "@owlprotocol/veraswap-sdk/artifacts/I
 import { atom } from "jotai";
 import { atomFamily } from "jotai/utils";
 import { getBalanceQueryOptions, readContractQueryOptions } from "wagmi/query";
-import { getAccount, GetBalanceReturnType } from "@wagmi/core";
+import { GetBalanceReturnType } from "@wagmi/core";
 import { atomWithQuery } from "jotai-tanstack-query";
 import { Address, zeroAddress } from "viem";
 import { Token } from "@owlprotocol/veraswap-sdk";
 import { accountAtom, tokensAtom } from "./index.js";
 import { config } from "@/config.js";
 
-export const tokenBalanceAtomFamily = atomFamily((token: Token) => {
-    const account = getAccount(config);
+export const tokenBalanceAtomFamily = atomFamily((token: Token) =>
+    atomWithQuery<number>((get) => {
+        const account = get(accountAtom);
+        const address = account?.address ?? zeroAddress;
+        const isConnected = !!account?.address;
 
-    if (token.standard === "NativeToken") {
-        return atomWithQuery(() => ({
-            ...getBalanceQueryOptions(config, { address: account.address ?? zeroAddress, chainId: token.chainId }),
-            enabled: !!account.address,
-            refetchInterval: 2000,
-            select: (data: GetBalanceReturnType) => Number(data.value) / 10 ** token.decimals,
-        }));
-    }
+        if (token.standard === "NativeToken") {
+            return {
+                ...(getBalanceQueryOptions(config, {
+                    address: address as Address,
+                    chainId: token.chainId,
+                }) as any),
+                enabled: isConnected,
+                select: (data: GetBalanceReturnType) => Number(data.value) / 10 ** token.decimals,
+            };
+        }
 
-    return atomWithQuery(() => ({
-        ...readContractQueryOptions(config, {
-            abi: [balanceOfAbi],
-            chainId: token.chainId,
-            address: token.address as Address,
-            functionName: "balanceOf",
-            args: [account.address ?? zeroAddress],
-        }),
-        enabled: !!token && !!account.address,
-        refetchInterval: 2000,
-        select: (data: bigint) => Number(data) / 10 ** token.decimals,
-    }));
-});
+        return {
+            ...(readContractQueryOptions(config, {
+                abi: [balanceOfAbi],
+                chainId: token.chainId,
+                address: token.address as Address,
+                functionName: "balanceOf",
+                args: [address],
+            }) as any),
+            enabled: isConnected,
+            select: (data: bigint) => Number(data) / 10 ** token.decimals,
+        };
+    }),
+);
 
 export const tokenBalancesAtom = atom((get) => {
     const allTokens = get(tokensAtom);
