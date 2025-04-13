dfe0d642d4b093b0798b59dc625e56fb42ff4224
Leo Vigna <leo.vigna@gmail.com>
2025-04-13 00:20:07 -0300

    add import plugin, import rules

--- packages/veraswap-sdk/eslint.config.mjs
+++ packages/veraswap-sdk/eslint.config.mjs
@@ -1,5 +1,37 @@
 import { typecheckedConfigs } from "@veraswap/eslint-config"
+import importPlugin from "eslint-plugin-import";
 
 export default [
     ...typecheckedConfigs,
+    importPlugin.flatConfigs.recommended,
+    importPlugin.flatConfigs.typescript,
+    {
+        rules: {
+            "import/extensions": ["warn", "ignorePackages", { json: "always" }],
+            "import/no-named-as-default": "off",
+            "import/no-commonjs": "error",
+            "import/no-default-export": "warn",
+            "import/no-anonymous-default-export": "error",
+            "import/no-cycle": "error",
+            "import/no-self-import": "error",
+            "import/no-unresolved": ["off", { ignore: [".js$"] }],
+            "import/no-internal-modules": [
+                "off",
+                {
+                    allow: [".json$"],
+                },
+            ],
+            "import/order": [
+                "warn",
+                {
+                    alphabetize: {
+                        order: "asc",
+                        caseInsensitive: true
+                    },
+                    named: true,
+                    "newlines-between": "always",
+                },
+            ]
+        }
+    }
 ]
