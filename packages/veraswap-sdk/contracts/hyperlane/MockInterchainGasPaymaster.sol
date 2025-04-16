9f5f685274bdb93a83a7974f9cad1a3cc6a5bc83
tota79 <balashyabni@gmail.com>
2025-04-16 17:25:52 -0300

    fix MockIGP

--- packages/veraswap-sdk/contracts/hyperlane/MockInterchainGasPaymaster.sol
+++ packages/veraswap-sdk/contracts/hyperlane/MockInterchainGasPaymaster.sol
@@ -8,11 +8,11 @@ import {IInterchainGasPaymaster} from "@hyperlane-xyz/core/interfaces/IInterchai
  * @dev Mock implementation of InterchainGasPaymaster for local testing
  */
 contract MockInterchainGasPaymaster is IInterchainGasPaymaster {
-    // 1 * 10^6 gas * 10^-9 eth (1 gwei) = 0.001 ether
-    uint256 public constant FIXED_GAS_PAYMENT = 0.001 ether;
+    // Fixed gas price of 1 gwei
+    uint256 public constant FIXED_GAS_PRICE = 1 gwei;
 
     function quoteGasPayment(uint32 _destinationDomain, uint256 _gasAmount) external pure override returns (uint256) {
-        return FIXED_GAS_PAYMENT;
+        return _gasAmount * FIXED_GAS_PRICE;
     }
 
     function payForGas(
