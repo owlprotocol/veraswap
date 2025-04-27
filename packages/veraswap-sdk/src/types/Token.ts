1357afebbbc369d13c4eb48a4887718d01635b86
Leo Vigna <leo.vigna@gmail.com>
2025-04-27 17:52:25 -0400

    delete token types, only leave TokenStandard

--- packages/veraswap-sdk/src/types/Token.ts
+++ packages/veraswap-sdk/src/types/Token.ts
@@ -1,5 +1,3 @@
-import { Address, zeroAddress } from "viem";
-
 /** Common Token Standards */
 export type TokenStandard =
     | "ERC20"
@@ -10,66 +8,3 @@ export type TokenStandard =
     | "HypSuperchainERC20Collateral"
     | "SuperchainERC20"
     | "NativeToken";
-
-/** Base Token interface with standard ERC20 metadata */
-export interface TokenBase<T extends string = TokenStandard> {
-    standard: T;
-    chainId: number;
-    address: Address;
-    decimals: number;
-    name: string;
-    symbol: string;
-    logoURI?: string;
-    connections?: {
-        vm: string;
-        chainId: number;
-        address: Address;
-    }[];
-}
-
-export interface NativeToken extends TokenBase<"NativeToken"> {
-    // TODO: find a better way to do this
-    address: typeof zeroAddress;
-}
-
-/** HypERC20 Token with remote connections */
-export interface HypERC20Token extends TokenBase<"HypERC20"> {
-    connections: {
-        vm: string;
-        chainId: number;
-        address: Address;
-    }[];
-}
-
-/** HypERC20Collateral Token with remote connections and collateral */
-export interface HypERC20CollateralToken extends TokenBase<"HypERC20Collateral"> {
-    collateralAddress: Address;
-    connections: {
-        vm: string;
-        chainId: number;
-        address: Address;
-    }[];
-}
-
-/**
- * HypSuperchainERC20Collateral Token with remote connections and collateral.
- * Assume that if the connection address it the same as the collateral address, then it's a Superchain connection
- * */
-export interface HypSuperchainERC20CollateralToken extends TokenBase<"HypSuperchainERC20Collateral"> {
-    collateralAddress: Address;
-    connections: {
-        vm: string;
-        chainId: number;
-        address: Address;
-    }[];
-}
-
-export type Token =
-    | TokenBase<"ERC20">
-    | TokenBase<"MockERC20">
-    | TokenBase<"MockSuperchainERC20">
-    | TokenBase<"SuperchainERC20">
-    | HypERC20Token
-    | HypERC20CollateralToken
-    | HypSuperchainERC20CollateralToken
-    | NativeToken;
