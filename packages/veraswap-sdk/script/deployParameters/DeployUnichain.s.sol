bbd9d6358e00ca91c04535e3ed7389514cef6da5
Oscar Baracos <oscar.baracos@gmail.com>
2025-04-29 17:31:42 +0200

    Fix unichain deploy params and add to configure mainnet

--- packages/veraswap-sdk/script/deployParameters/DeployUnichain.s.sol
+++ packages/veraswap-sdk/script/deployParameters/DeployUnichain.s.sol
@@ -18,9 +18,9 @@ library DeployUnichain {
             v4PoolManager: 0x1F98400000000000000000000000000000000004,
             v3NFTPositionManager: 0x943e6e07a7E8E791dAFC44083e54041D743C46E9,
             v4PositionManager: 0x4529A01c7A0410167c5740C487A8DE60232617bf,
-            v4StateView: 0x86e8631A016F9068C3f085fAF484Ee3F5fDee8f2,
-            v4Quoter: 0x333E3C607B141b18fF6de9f258db6e77fE7491E0,
-            universalRouter: 0xEf740bf23aCaE26f6492B10de645D6B98dC8Eaf3
+            v4StateView: address(0),
+            v4Quoter: address(0),
+            universalRouter: address(0)
         });
 
         HyperlaneDeployParams memory hyperlane = HyperlaneDeployParams({mailbox: 0x3a464f746D23Ab22155710f44dB16dcA53e0775E});
