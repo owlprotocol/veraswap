c0444e73ab6dba7083b1b975f4ca05ff54722cf6
Oscar Baracos <oscar.baracos@gmail.com>
2025-04-29 20:35:14 +0200

    Add default tick spacing for create pool key (#192)
    * Add default tick spacing for create pool key
    
    * simpler, default params
    
    ---------
    
    Co-authored-by: Leo Vigna <leo.vigna@gmail.com>

--- packages/veraswap-sdk/src/types/PoolKey.ts
+++ packages/veraswap-sdk/src/types/PoolKey.ts
@@ -1,4 +1,4 @@
-import { Address, encodeAbiParameters, keccak256 } from "viem";
+import { Address, encodeAbiParameters, keccak256, zeroAddress } from "viem";
 
 export interface PoolKey {
     currency0: Address;
@@ -24,11 +24,34 @@ export function getPoolId(poolKey: PoolKey) {
     return keccak256(encodeAbiParameters([PoolKeyAbi], [poolKey]));
 }
 
+export const DEFAULT_POOL_PARAMS = {
+    FEE_100_TICK_1: {
+        fee: 100,
+        tickSpacing: 1,
+        hooks: zeroAddress,
+    },
+    FEE_500_TICK_10: {
+        fee: 500,
+        tickSpacing: 10,
+        hooks: zeroAddress,
+    },
+    FEE_3000_TICK_60: {
+        fee: 3000,
+        tickSpacing: 60,
+        hooks: zeroAddress,
+    },
+    FEE_10_000_TICK_200: {
+        fee: 10_000,
+        tickspacing: 200,
+        hooks: zeroAddress,
+    },
+};
+
 /**
  * Create pool key with validation
  * @param poolKey
  */
-export function createPoolKey(poolKey: PoolKey) {
+export function createPoolKey(poolKey: PoolKey): PoolKey {
     return {
         ...poolKey,
         currency0: poolKey.currency0 < poolKey.currency1 ? poolKey.currency0 : poolKey.currency1,
