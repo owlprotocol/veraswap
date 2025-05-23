b60163d79c191a35ca82d43a688d7599e3006289
Leo Vigna <leo.vigna@gmail.com>
2025-05-23 14:32:05 -0700

    V4Quoter tests all

--- packages/veraswap-sdk/test/V4QuoterTest.s.sol
+++ packages/veraswap-sdk/test/V4QuoterTest.s.sol
@@ -22,6 +22,7 @@ import {IStateView} from "@uniswap/v4-periphery/src/interfaces/IStateView.sol";
 import {IV4Quoter} from "@uniswap/v4-periphery/src/interfaces/IV4Quoter.sol";
 import {IV4Router} from "@uniswap/v4-periphery/src/interfaces/IV4Router.sol";
 import {Actions} from "@uniswap/v4-periphery/src/libraries/Actions.sol";
+import {PathKey} from "@uniswap/v4-periphery/src/libraries/PathKey.sol";
 import {StateViewUtils} from "../script/utils/StateViewUtils.sol";
 import {V4QuoterUtils} from "../script/utils/V4QuoterUtils.sol";
 // Uniswap Universal Router
@@ -37,6 +38,13 @@ contract V4QuoterTest is Test {
     bytes32 constant BYTES32_ZERO = bytes32(0);
     uint128 constant amount = 0.01 ether;
 
+    struct PoolKeyOptions {
+        uint24 fee;
+        int24 tickSpacing;
+        IHooks hooks;
+    }
+    PoolKeyOptions internal poolKeyOptions;
+
     // Tokens
     IERC20 internal tokenA;
     IERC20 internal tokenB;
@@ -50,8 +58,10 @@ contract V4QuoterTest is Test {
     IUniversalRouter internal router;
 
     function setUp() public {
-        vm.startBroadcast(); // Sets proper address for Create2 & transaction sender
-
+        // Global poolKey options
+        poolKeyOptions = PoolKeyOptions({fee: 3000, tickSpacing: 60, hooks: IHooks(address(0))});
+        // Sets proper address for Create2 & transaction sender
+        vm.startBroadcast();
         // Tokens
         (address _tokenA, ) = MockERC20Utils.getOrCreate2("Token A", "A", 18);
         (address _tokenB, ) = MockERC20Utils.getOrCreate2("Token B", "B", 18);
@@ -98,20 +108,22 @@ contract V4QuoterTest is Test {
 
     // A (exact) -> ETH (variable)
     function testExactInputSingle() public {
+        // Currency
+        (Currency currencyIn, Currency currencyOut) = (Currency.wrap(address(tokenA)), Currency.wrap(address(0)));
         // PoolKey
-        (Currency currency0, Currency currency1) = address(0) < address(tokenA)
-            ? (Currency.wrap(address(0)), Currency.wrap(address(tokenA)))
-            : (Currency.wrap(address(tokenA)), Currency.wrap(address(0)));
+        (Currency currency0, Currency currency1) = currencyIn < currencyOut
+            ? (currencyIn, currencyOut)
+            : (currencyOut, currencyIn);
         PoolKey memory poolKey = PoolKey({
             currency0: currency0,
             currency1: currency1,
-            fee: 3000,
-            tickSpacing: 60,
-            hooks: IHooks(address(0))
+            fee: poolKeyOptions.fee,
+            tickSpacing: poolKeyOptions.tickSpacing,
+            hooks: poolKeyOptions.hooks
         });
 
         // V4 Quote
-        bool zeroForOne = currency0 == Currency.wrap(address(tokenA));
+        bool zeroForOne = currency0 == currencyIn;
         assertFalse(zeroForOne);
         IV4Quoter.QuoteExactSingleParams memory v4QuoteParams = IV4Quoter.QuoteExactSingleParams({
             poolKey: poolKey,
@@ -137,10 +149,7 @@ contract V4QuoterTest is Test {
                 amountOutMinimum: uint128(amountOut),
                 hookData: v4QuoteParams.hookData
             })
-        ); // Swap
-        (Currency currencyIn, Currency currencyOut) = zeroForOne
-            ? (v4QuoteParams.poolKey.currency0, v4QuoteParams.poolKey.currency1)
-            : (v4QuoteParams.poolKey.currency1, v4QuoteParams.poolKey.currency0);
+        );
         v4ActionParams[1] = abi.encode(currencyIn, v4QuoteParams.exactAmount); // Settle input
         v4ActionParams[2] = abi.encode(currencyOut, amountOut); // Take output
         // Encode Universal Router Commands
@@ -154,26 +163,28 @@ contract V4QuoterTest is Test {
         router.execute(routerCommands, routerCommandInputs, deadline);
         uint256 currencyInBalanceAfterSwap = currencyIn.balanceOf(msg.sender);
         uint256 currencyOutBalanceAfterSwap = currencyOut.balanceOf(msg.sender);
-        assertEq(currencyInBalanceAfterSwap, currencyInBalanceBeforeSwap - v4QuoteParams.exactAmount); // Input balance decreased exactly by amount
-        assertGt(currencyOutBalanceAfterSwap, currencyOutBalanceBeforeSwap); // Output balance increased (can't check exact amount due to gas cost)
+        assertEq(currencyInBalanceAfterSwap, currencyInBalanceBeforeSwap - v4QuoteParams.exactAmount); // Input balance decreased by exact amount
+        assertGt(currencyOutBalanceAfterSwap, currencyOutBalanceBeforeSwap); // Output balance increased (can't check amount due to gas cost)
     }
 
     // A (variable) -> ETH (exact)
     function testExactOutputSingle() public {
+        // Currency
+        (Currency currencyIn, Currency currencyOut) = (Currency.wrap(address(tokenA)), Currency.wrap(address(0)));
         // PoolKey
-        (Currency currency0, Currency currency1) = address(0) < address(tokenA)
-            ? (Currency.wrap(address(0)), Currency.wrap(address(tokenA)))
-            : (Currency.wrap(address(tokenA)), Currency.wrap(address(0)));
+        (Currency currency0, Currency currency1) = currencyIn < currencyOut
+            ? (currencyIn, currencyOut)
+            : (currencyOut, currencyIn);
         PoolKey memory poolKey = PoolKey({
             currency0: currency0,
             currency1: currency1,
-            fee: 3000,
-            tickSpacing: 60,
-            hooks: IHooks(address(0))
+            fee: poolKeyOptions.fee,
+            tickSpacing: poolKeyOptions.tickSpacing,
+            hooks: poolKeyOptions.hooks
         });
 
         // V4 Quote
-        bool zeroForOne = currency0 == Currency.wrap(address(tokenA));
+        bool zeroForOne = currency0 == currencyIn;
         assertFalse(zeroForOne);
         IV4Quoter.QuoteExactSingleParams memory v4QuoteParams = IV4Quoter.QuoteExactSingleParams({
             poolKey: poolKey,
@@ -199,10 +210,7 @@ contract V4QuoterTest is Test {
                 amountInMaximum: uint128(amountIn),
                 hookData: v4QuoteParams.hookData
             })
-        ); // Swap
-        (Currency currencyIn, Currency currencyOut) = zeroForOne
-            ? (v4QuoteParams.poolKey.currency0, v4QuoteParams.poolKey.currency1)
-            : (v4QuoteParams.poolKey.currency1, v4QuoteParams.poolKey.currency0);
+        );
         v4ActionParams[1] = abi.encode(currencyIn, amountIn); // Settle input
         v4ActionParams[2] = abi.encode(currencyOut, v4QuoteParams.exactAmount); // Take output
         // Encode Universal Router Commands
@@ -216,17 +224,133 @@ contract V4QuoterTest is Test {
         router.execute(routerCommands, routerCommandInputs, deadline);
         uint256 currencyInBalanceAfterSwap = currencyIn.balanceOf(msg.sender);
         uint256 currencyOutBalanceAfterSwap = currencyOut.balanceOf(msg.sender);
-        assertEq(currencyInBalanceAfterSwap, currencyInBalanceBeforeSwap - amountIn); // Input balance decreased exactly by amount
-        assertGt(currencyOutBalanceAfterSwap, currencyOutBalanceBeforeSwap); // Output balance increased (can't check exact amount due to gas cost)
+        assertEq(currencyInBalanceAfterSwap, currencyInBalanceBeforeSwap - amountIn); // Input balance decreased by quote amount
+        assertGt(currencyOutBalanceAfterSwap, currencyOutBalanceBeforeSwap); // Output balance increased (can't check amount due to gas cost)
     }
 
     // A (exact) -> ETH -> B (variable)
-    function testExactInput() public pure {
-        assertEq(true, true);
+    function testExactInput() public {
+        // Currency
+        (Currency currencyIn, Currency currencyOut) = (Currency.wrap(address(tokenA)), Currency.wrap(address(tokenB)));
+        // V4 Quote
+        PathKey[] memory path = new PathKey[](2);
+        PathKey memory pathKeyIntermediate = PathKey({
+            intermediateCurrency: Currency.wrap(address(0)),
+            fee: poolKeyOptions.fee,
+            tickSpacing: poolKeyOptions.tickSpacing,
+            hooks: poolKeyOptions.hooks,
+            hookData: ""
+        });
+        PathKey memory pathKeyOutput = PathKey({
+            intermediateCurrency: currencyOut,
+            fee: poolKeyOptions.fee,
+            tickSpacing: poolKeyOptions.tickSpacing,
+            hooks: poolKeyOptions.hooks,
+            hookData: ""
+        });
+        path[0] = pathKeyIntermediate;
+        path[1] = pathKeyOutput;
+
+        IV4Quoter.QuoteExactParams memory v4QuoteParams = IV4Quoter.QuoteExactParams({
+            exactCurrency: currencyIn,
+            path: path,
+            exactAmount: amount
+        });
+        (uint256 amountOut, ) = v4Quoter.quoteExactInput(v4QuoteParams);
+        assertGt(amountOut, 0);
+        // V4 Swap
+        // Encode V4 Swap Actions
+        bytes memory v4Actions = abi.encodePacked(
+            uint8(Actions.SWAP_EXACT_IN),
+            uint8(Actions.SETTLE_ALL),
+            uint8(Actions.TAKE_ALL)
+        );
+        bytes[] memory v4ActionParams = new bytes[](3);
+        v4ActionParams[0] = abi.encode(
+            IV4Router.ExactInputParams({
+                currencyIn: v4QuoteParams.exactCurrency,
+                path: path,
+                amountIn: v4QuoteParams.exactAmount,
+                amountOutMinimum: uint128(amountOut)
+            })
+        ); // Swap
+        v4ActionParams[1] = abi.encode(currencyIn, v4QuoteParams.exactAmount); // Settle input
+        v4ActionParams[2] = abi.encode(currencyOut, amountOut); // Take output
+        // Encode Universal Router Commands
+        bytes memory routerCommands = abi.encodePacked(uint8(Commands.V4_SWAP));
+        bytes[] memory routerCommandInputs = new bytes[](1);
+        routerCommandInputs[0] = abi.encode(v4Actions, v4ActionParams);
+        // Execute Swap
+        uint256 currencyInBalanceBeforeSwap = currencyIn.balanceOf(msg.sender);
+        uint256 currencyOutBalanceBeforeSwap = currencyOut.balanceOf(msg.sender);
+        uint256 deadline = block.timestamp + 20;
+        router.execute(routerCommands, routerCommandInputs, deadline);
+        uint256 currencyInBalanceAfterSwap = currencyIn.balanceOf(msg.sender);
+        uint256 currencyOutBalanceAfterSwap = currencyOut.balanceOf(msg.sender);
+        assertEq(currencyInBalanceAfterSwap, currencyInBalanceBeforeSwap - v4QuoteParams.exactAmount); // Input balance decreased by exact amount
+        assertEq(currencyOutBalanceAfterSwap, currencyOutBalanceBeforeSwap + amountOut); // Output balance increased by variable amount
     }
 
     // A (variable) -> ETH -> B (exact)
-    function testExactOutput() public pure {
-        assertEq(true, true);
+    function testExactOutput() public {
+        // Currency
+        (Currency currencyIn, Currency currencyOut) = (Currency.wrap(address(tokenA)), Currency.wrap(address(tokenB)));
+        // V4 Quote
+        PathKey[] memory path = new PathKey[](2);
+        PathKey memory pathKeyInput = PathKey({
+            intermediateCurrency: currencyIn,
+            fee: poolKeyOptions.fee,
+            tickSpacing: poolKeyOptions.tickSpacing,
+            hooks: poolKeyOptions.hooks,
+            hookData: ""
+        });
+        PathKey memory pathKeyIntermediate = PathKey({
+            intermediateCurrency: Currency.wrap(address(0)),
+            fee: poolKeyOptions.fee,
+            tickSpacing: poolKeyOptions.tickSpacing,
+            hooks: poolKeyOptions.hooks,
+            hookData: ""
+        });
+        path[0] = pathKeyInput;
+        path[1] = pathKeyIntermediate;
+
+        IV4Quoter.QuoteExactParams memory v4QuoteParams = IV4Quoter.QuoteExactParams({
+            exactCurrency: currencyOut,
+            path: path,
+            exactAmount: amount
+        });
+        (uint256 amountIn, ) = v4Quoter.quoteExactOutput(v4QuoteParams);
+        assertGt(amountIn, 0);
+        // V4 Swap
+        // Encode V4 Swap Actions
+        bytes memory v4Actions = abi.encodePacked(
+            uint8(Actions.SWAP_EXACT_OUT),
+            uint8(Actions.SETTLE_ALL),
+            uint8(Actions.TAKE_ALL)
+        );
+        bytes[] memory v4ActionParams = new bytes[](3);
+        v4ActionParams[0] = abi.encode(
+            IV4Router.ExactOutputParams({
+                currencyOut: v4QuoteParams.exactCurrency,
+                path: path,
+                amountOut: v4QuoteParams.exactAmount,
+                amountInMaximum: uint128(amountIn)
+            })
+        ); // Swap
+        v4ActionParams[1] = abi.encode(currencyIn, amountIn); // Settle input
+        v4ActionParams[2] = abi.encode(currencyOut, v4QuoteParams.exactAmount); // Take output
+        // Encode Universal Router Commands
+        bytes memory routerCommands = abi.encodePacked(uint8(Commands.V4_SWAP));
+        bytes[] memory routerCommandInputs = new bytes[](1);
+        routerCommandInputs[0] = abi.encode(v4Actions, v4ActionParams);
+        // Execute Swap
+        uint256 currencyInBalanceBeforeSwap = currencyIn.balanceOf(msg.sender);
+        uint256 currencyOutBalanceBeforeSwap = currencyOut.balanceOf(msg.sender);
+        uint256 deadline = block.timestamp + 20;
+        router.execute(routerCommands, routerCommandInputs, deadline);
+        uint256 currencyInBalanceAfterSwap = currencyIn.balanceOf(msg.sender);
+        uint256 currencyOutBalanceAfterSwap = currencyOut.balanceOf(msg.sender);
+        assertEq(currencyInBalanceAfterSwap, currencyInBalanceBeforeSwap - amountIn); // Input balance decreased by variable amount
+        assertEq(currencyOutBalanceAfterSwap, currencyOutBalanceBeforeSwap + v4QuoteParams.exactAmount); // Output balance increased by exact amount
     }
 }
