c99ebcd657769ccab241e679c0e95f72d6c2b5e9
Leo Vigna <leo.vigna@gmail.com>
2025-05-25 02:59:33 -0700

    remove base interfaces

--- packages/veraswap-sdk/contracts/uniswap/v3/V3MetaQuoterBase.sol
+++ packages/veraswap-sdk/contracts/uniswap/v3/V3MetaQuoterBase.sol
@@ -8,21 +8,24 @@ import {QuoterRevert} from "@uniswap/v4-periphery/src/libraries/QuoterRevert.sol
 import {V3QuoterBase} from "./V3QuoterBase.sol";
 import {V3PoolKey} from "./V3PoolKey.sol";
 import {V3PathKey} from "./V3PathKey.sol";
-import {IV3MetaQuoterBase} from "./IV3MetaQuoterBase.sol";
+import {IV3Quoter} from "./IV3Quoter.sol";
+import {IV3MetaQuoter} from "./IV3MetaQuoter.sol";
 
 /// @title V3MetaQuoterBase
-abstract contract V3MetaQuoterBase is IV3MetaQuoterBase, V3QuoterBase {
+abstract contract V3MetaQuoterBase is V3QuoterBase {
     using QuoterRevert for *;
     using ParseBytes for bytes;
 
     constructor(address _factory, bytes32 _poolInitCodeHash) V3QuoterBase(_factory, _poolInitCodeHash) {}
 
     function _metaQuoteExactInputSingle(
-        MetaQuoteExactSingleParams memory params
-    ) internal returns (MetaQuoteExactSingleResult[] memory swaps) {
+        IV3MetaQuoter.MetaQuoteExactSingleParams memory params
+    ) internal returns (IV3MetaQuoter.MetaQuoteExactSingleResult[] memory swaps) {
         uint256 quoteResultsMaxLen = params.feeOptions.length;
         uint256 quoteResultsCount = 0;
-        MetaQuoteExactSingleResult[] memory quoteResults = new MetaQuoteExactSingleResult[](quoteResultsMaxLen);
+        IV3MetaQuoter.MetaQuoteExactSingleResult[] memory quoteResults = new IV3MetaQuoter.MetaQuoteExactSingleResult[](
+            quoteResultsMaxLen
+        );
 
         (Currency currency0, Currency currency1) = params.exactCurrency < params.variableCurrency
             ? (params.exactCurrency, params.variableCurrency)
@@ -36,7 +39,7 @@ abstract contract V3MetaQuoterBase is IV3MetaQuoterBase, V3QuoterBase {
 
             V3PoolKey memory poolKey = V3PoolKey({currency0: currency0, currency1: currency1, fee: fee});
 
-            QuoteExactSingleParams memory quoteParams = QuoteExactSingleParams({
+            IV3Quoter.QuoteExactSingleParams memory quoteParams = IV3Quoter.QuoteExactSingleParams({
                 poolKey: poolKey,
                 zeroForOne: zeroForOne,
                 exactAmount: params.exactAmount
@@ -50,7 +53,7 @@ abstract contract V3MetaQuoterBase is IV3MetaQuoterBase, V3QuoterBase {
             quoteResultsCount++;
             uint256 variableAmount = reason.parseQuoteAmount();
 
-            MetaQuoteExactSingleResult memory quote = MetaQuoteExactSingleResult({
+            IV3MetaQuoter.MetaQuoteExactSingleResult memory quote = IV3MetaQuoter.MetaQuoteExactSingleResult({
                 poolKey: poolKey,
                 zeroForOne: zeroForOne,
                 variableAmount: variableAmount,
@@ -60,7 +63,7 @@ abstract contract V3MetaQuoterBase is IV3MetaQuoterBase, V3QuoterBase {
         }
 
         // Filter out empty results
-        swaps = new MetaQuoteExactSingleResult[](quoteResultsCount);
+        swaps = new IV3MetaQuoter.MetaQuoteExactSingleResult[](quoteResultsCount);
         uint256 swapsIndex = 0;
         for (uint256 i = 0; i < quoteResults.length; i++) {
             if (quoteResults[i].gasEstimate != 0) {
@@ -71,11 +74,13 @@ abstract contract V3MetaQuoterBase is IV3MetaQuoterBase, V3QuoterBase {
     }
 
     function _metaQuoteExactOutputSingle(
-        MetaQuoteExactSingleParams memory params
-    ) internal returns (MetaQuoteExactSingleResult[] memory swaps) {
+        IV3MetaQuoter.MetaQuoteExactSingleParams memory params
+    ) internal returns (IV3MetaQuoter.MetaQuoteExactSingleResult[] memory swaps) {
         uint256 quoteResultsMaxLen = params.feeOptions.length;
         uint256 quoteResultsCount = 0;
-        MetaQuoteExactSingleResult[] memory quoteResults = new MetaQuoteExactSingleResult[](quoteResultsMaxLen);
+        IV3MetaQuoter.MetaQuoteExactSingleResult[] memory quoteResults = new IV3MetaQuoter.MetaQuoteExactSingleResult[](
+            quoteResultsMaxLen
+        );
 
         (Currency currency0, Currency currency1) = params.exactCurrency < params.variableCurrency
             ? (params.exactCurrency, params.variableCurrency)
@@ -89,7 +94,7 @@ abstract contract V3MetaQuoterBase is IV3MetaQuoterBase, V3QuoterBase {
 
             V3PoolKey memory poolKey = V3PoolKey({currency0: currency0, currency1: currency1, fee: fee});
 
-            QuoteExactSingleParams memory quoteParams = QuoteExactSingleParams({
+            IV3Quoter.QuoteExactSingleParams memory quoteParams = IV3Quoter.QuoteExactSingleParams({
                 poolKey: poolKey,
                 zeroForOne: zeroForOne,
                 exactAmount: params.exactAmount
@@ -103,7 +108,7 @@ abstract contract V3MetaQuoterBase is IV3MetaQuoterBase, V3QuoterBase {
             quoteResultsCount++;
             uint256 variableAmount = reason.parseQuoteAmount();
 
-            MetaQuoteExactSingleResult memory quote = MetaQuoteExactSingleResult({
+            IV3MetaQuoter.MetaQuoteExactSingleResult memory quote = IV3MetaQuoter.MetaQuoteExactSingleResult({
                 poolKey: poolKey,
                 zeroForOne: zeroForOne,
                 variableAmount: variableAmount,
@@ -113,7 +118,7 @@ abstract contract V3MetaQuoterBase is IV3MetaQuoterBase, V3QuoterBase {
         }
 
         // Filter out empty results
-        swaps = new MetaQuoteExactSingleResult[](quoteResultsCount);
+        swaps = new IV3MetaQuoter.MetaQuoteExactSingleResult[](quoteResultsCount);
         uint256 swapsIndex = 0;
         for (uint256 i = 0; i < quoteResults.length; i++) {
             if (quoteResults[i].gasEstimate != 0) {
