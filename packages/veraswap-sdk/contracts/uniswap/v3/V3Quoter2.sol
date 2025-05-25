a10ec6594565890d55123fe6656552f3a8c6ed9c
Leo Vigna <leo.vigna@gmail.com>
2025-05-24 23:46:20 -0700

    v3Quoter2 multihop quote

--- packages/veraswap-sdk/contracts/uniswap/v3/V3Quoter2.sol
+++ packages/veraswap-sdk/contracts/uniswap/v3/V3Quoter2.sol
@@ -6,10 +6,11 @@ import {IUniswapV3SwapCallback} from "@uniswap/v3-core/contracts/interfaces/call
 
 import {TickMath} from "@uniswap/v4-core/src/libraries/TickMath.sol";
 import {QuoterRevert} from "@uniswap/v4-periphery/src/libraries/QuoterRevert.sol";
+import {Currency} from "@uniswap/v4-core/src/types/Currency.sol";
 
 import {V3CallbackValidation} from "./V3CallbackValidation.sol";
 import {IV3Quoter} from "./IV3Quoter.sol";
-import {V3PoolKey} from "./V3PoolKey.sol";
+import {V3PoolKey, V3PoolKeyLibrary} from "./V3PoolKey.sol";
 
 contract V3Quoter2 is IV3Quoter, IUniswapV3SwapCallback {
     using QuoterRevert for *;
@@ -50,7 +51,7 @@ contract V3Quoter2 is IV3Quoter, IUniswapV3SwapCallback {
     /// @inheritdoc IV3Quoter
     function quoteExactInputSingle(
         QuoteExactSingleParams memory params
-    ) external returns (uint256 amountOut, uint256 gasEstimate) {
+    ) public returns (uint256 amountOut, uint256 gasEstimate) {
         bool zeroForOne = params.zeroForOne;
         IUniswapV3Pool pool = IUniswapV3Pool(params.poolKey.computeAddress(factory, poolInitCodeHash));
 
@@ -72,14 +73,42 @@ contract V3Quoter2 is IV3Quoter, IUniswapV3SwapCallback {
     }
 
     /// @inheritdoc IV3Quoter
-    function quoteExactInput(
-        QuoteExactParams memory params
-    ) external returns (uint256 amountOut, uint256 gasEstimate) {}
+    function quoteExactInput(QuoteExactParams memory params) external returns (uint256 amountOut, uint256 gasEstimate) {
+        Currency exactCurrency = params.exactCurrency;
+        uint128 exactAmount = params.exactAmount;
+
+        uint256 idx = 0;
+        while (true) {
+            V3PoolKey memory poolKey = V3PoolKeyLibrary.getPoolKey(
+                exactCurrency,
+                params.path[idx].intermediateCurrency,
+                params.path[idx].fee
+            );
+
+            QuoteExactSingleParams memory quoteParams = QuoteExactSingleParams({
+                poolKey: poolKey,
+                exactAmount: exactAmount,
+                zeroForOne: exactCurrency == poolKey.currency0
+            });
+            (uint256 amount, uint256 gas) = quoteExactInputSingle(quoteParams);
+            gasEstimate += gas;
+
+            if (idx == params.path.length - 1) {
+                // last path element, return final amount
+                return (amount, gasEstimate);
+            } else {
+                // update the exact currency and amount for the next iteration
+                exactCurrency = params.path[idx].intermediateCurrency;
+                exactAmount = uint128(amount);
+                idx++;
+            }
+        }
+    }
 
     /// @inheritdoc IV3Quoter
     function quoteExactOutputSingle(
         QuoteExactSingleParams memory params
-    ) external returns (uint256 amountIn, uint256 gasEstimate) {
+    ) public returns (uint256 amountIn, uint256 gasEstimate) {
         bool zeroForOne = params.zeroForOne;
         IUniswapV3Pool pool = IUniswapV3Pool(params.poolKey.computeAddress(factory, poolInitCodeHash));
 
@@ -101,7 +130,35 @@ contract V3Quoter2 is IV3Quoter, IUniswapV3SwapCallback {
     }
 
     /// @inheritdoc IV3Quoter
-    function quoteExactOutput(
-        QuoteExactParams memory params
-    ) external returns (uint256 amountIn, uint256 gasEstimate) {}
+    function quoteExactOutput(QuoteExactParams memory params) external returns (uint256 amountIn, uint256 gasEstimate) {
+        Currency exactCurrency = params.exactCurrency;
+        uint128 exactAmount = params.exactAmount;
+
+        uint256 idx = params.path.length - 1;
+        while (true) {
+            V3PoolKey memory poolKey = V3PoolKeyLibrary.getPoolKey(
+                exactCurrency,
+                params.path[idx].intermediateCurrency,
+                params.path[idx].fee
+            );
+
+            QuoteExactSingleParams memory quoteParams = QuoteExactSingleParams({
+                poolKey: poolKey,
+                exactAmount: exactAmount,
+                zeroForOne: exactCurrency == poolKey.currency0
+            });
+            (uint256 amount, uint256 gas) = quoteExactOutputSingle(quoteParams);
+            gasEstimate += gas;
+
+            if (idx == 0) {
+                // last path element, return final amount
+                return (amount, gasEstimate);
+            } else {
+                // update the exact currency and amount for the next iteration
+                exactCurrency = params.path[idx].intermediateCurrency;
+                exactAmount = uint128(amount);
+                idx--;
+            }
+        }
+    }
 }
