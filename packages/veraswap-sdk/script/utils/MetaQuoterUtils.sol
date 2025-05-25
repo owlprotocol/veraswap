9cd53ba4811ef35ab0d2bc33dcd991140613e53e
Leo Vigna <leo.vigna@gmail.com>
2025-05-25 03:58:48 -0700

    initial MetaQuoter test

---
+++ packages/veraswap-sdk/script/utils/MetaQuoterUtils.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.26;
+
+import {Vm} from "forge-std/Vm.sol";
+
+import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import {MetaQuoter} from "../../contracts/uniswap/MetaQuoter.sol";
+import {Create2Utils} from "./Create2Utils.sol";
+
+library MetaQuoterUtils {
+    Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+
+    function getDeployBytecode(
+        address factory,
+        bytes32 poolInitCodeHash,
+        address poolManager
+    ) internal pure returns (bytes memory) {
+        return abi.encodePacked(type(MetaQuoter).creationCode, abi.encode(factory, poolInitCodeHash, poolManager));
+    }
+
+    function getOrCreate2(
+        address factory,
+        bytes32 poolInitCodeHash,
+        address poolManager
+    ) internal returns (address addr, bool exists) {
+        (addr, exists) = Create2Utils.getAddressExists(getDeployBytecode(factory, poolInitCodeHash, poolManager));
+        if (!exists) {
+            address deployed = address(
+                new MetaQuoter{salt: Create2Utils.BYTES32_ZERO}(factory, poolInitCodeHash, IPoolManager(poolManager))
+            );
+            vm.assertEq(deployed, addr);
+        }
+    }
+}
