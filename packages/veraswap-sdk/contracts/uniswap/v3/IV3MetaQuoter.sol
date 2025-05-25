3ba48ea20d6abb71a61ecc6b7d397e636a974812
Leo Vigna <leo.vigna@gmail.com>
2025-05-25 02:23:09 -0700

    V3MetaQuoter compute best, use V3MetaQuoterBase

--- packages/veraswap-sdk/contracts/uniswap/v3/IV3MetaQuoter.sol
+++ packages/veraswap-sdk/contracts/uniswap/v3/IV3MetaQuoter.sol
@@ -4,21 +4,15 @@ pragma solidity ^0.8.0;
 import {Currency} from "@uniswap/v4-core/src/types/Currency.sol";
 import {V3PoolKey} from "./V3PoolKey.sol";
 import {V3PathKey} from "./V3PathKey.sol";
+import {IV3MetaQuoterBase} from "./IV3MetaQuoterBase.sol";
 
 /// @title IV3MetaQuoter
 /// @notice Interface for MetaQuoter, a contract that searches for multiple quotes
-interface IV3MetaQuoter {
+interface IV3MetaQuoter is IV3MetaQuoterBase {
     enum BestSwap {
         None,
         Single,
         Multihop
-    }
-
-    struct MetaQuoteExactSingleParams {
-        Currency exactCurrency;
-        Currency variableCurrency;
-        uint128 exactAmount;
-        uint24[] feeOptions;
     }
 
     struct MetaQuoteExactParams {
@@ -27,13 +21,6 @@ interface IV3MetaQuoter {
         Currency[] hopCurrencies;
         uint128 exactAmount;
         uint24[] feeOptions;
-    }
-
-    struct MetaQuoteExactSingleResult {
-        V3PoolKey poolKey;
-        bool zeroForOne;
-        uint256 variableAmount; // variable amountIn or amountOut from quoteExactSingle
-        uint256 gasEstimate; // gas estimate from quoteExact
     }
 
     struct MetaQuoteExactResult {
