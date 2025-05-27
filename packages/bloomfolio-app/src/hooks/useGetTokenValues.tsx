434b54b01389dbab9d19f2548c70bf6855277e31
tota79 <balashyabni@gmail.com>
2025-05-27 17:49:38 +0200

    refetch quote

--- packages/bloomfolio-app/src/hooks/useGetTokenValues.tsx
+++ packages/bloomfolio-app/src/hooks/useGetTokenValues.tsx
@@ -12,13 +12,15 @@ export function useGetTokenValues({
     basketDetails: readonly { addr: Address; units: bigint }[];
 }) {
     return useQueries({
-        queries: basketDetails.map((allocation) =>
-            // TODO: cast return type
-            getTokenDollarValueQueryOptions(config, {
+        queries: basketDetails.map((allocation) => ({
+            ...getTokenDollarValueQueryOptions(config, {
                 tokenAddress: allocation.addr,
                 chainId,
             }),
-        ),
+            refetchInterval: 2000,
+            refetchOnWindowFocus: false,
+            staleTime: 2000,
+        })),
         combine: (results) => ({
             data: results.map((result) => {
                 if (!result.data) return 0n;
