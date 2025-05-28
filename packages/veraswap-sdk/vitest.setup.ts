1da0e9ed7fde14e0db36829f2d5c1e8778013536
Oscar Baracos <oscar.baracos@gmail.com>
2025-05-28 16:58:02 +0200

    Add code-size-limit to vitest deploy script

--- packages/veraswap-sdk/vitest.setup.ts
+++ packages/veraswap-sdk/vitest.setup.ts
@@ -7,14 +7,27 @@ import { exec } from "node:child_process";
 import { entryPoint07Address } from "viem/account-abstraction";
 import { getAnvilAccount } from "@veraswap/anvil-account";
 import { ENTRYPOINT_SALT_V07 } from "@owlprotocol/contracts-account-abstraction";
-import { EntryPoint } from "@owlprotocol/contracts-account-abstraction/artifacts/EntryPoint"
-import { getOrDeployDeterministicContract } from "@veraswap/create-deterministic"
+import { EntryPoint } from "@owlprotocol/contracts-account-abstraction/artifacts/EntryPoint";
+import { getOrDeployDeterministicContract } from "@veraswap/create-deterministic";
 import { OpenPaymaster } from "./src/artifacts/OpenPaymaster.js";
 import { BalanceDeltaPaymaster } from "./src/artifacts/BalanceDeltaPaymaster.js";
 import { ERC4337_CONTRACTS } from "./src/constants/erc4337.js";
 
-import { opChainL1, opChainL1Port, opChainA, opChainAPort, opChainB, opChainBPort, opChainABundlerPort, opChainBBundlerPort, opChainL1BundlerPort, opChainL1Client, opChainAClient, opChainBClient } from "./src/chains/index.js";
-import { createWalletClient, Hex, http, parseEther, zeroAddress } from "viem";
+import {
+    opChainL1,
+    opChainL1Port,
+    opChainA,
+    opChainAPort,
+    opChainB,
+    opChainBPort,
+    opChainABundlerPort,
+    opChainBBundlerPort,
+    opChainL1BundlerPort,
+    opChainL1Client,
+    opChainAClient,
+    opChainBClient,
+} from "./src/chains/index.js";
+import { createWalletClient, Hex, http, parseEther } from "viem";
 
 const execPromise = promisify(exec);
 
@@ -53,9 +66,9 @@ export async function setup() {
     await chainBInstance.start();
 
     // Private Keys
-    const anvil0 = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
-    const anvil1 = "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"
-    const anvil2 = "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a"
+    const anvil0 = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+    const anvil1 = "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d";
+    const anvil2 = "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a";
 
     // Deploy EntryPoint artifact
     const anvilAccount = getAnvilAccount();
@@ -65,20 +78,22 @@ export async function setup() {
             chain: client.chain,
             transport: http(),
         });
-        const entryPointDeploy = await getOrDeployDeterministicContract(anvilClient,
+        const entryPointDeploy = await getOrDeployDeterministicContract(
+            anvilClient,
             //Extracted salt (first 32 bytes) from original tx
             //https://etherscan.io/tx/0x5c81ea86f6c54481d3e21c78675b4f1d985c1fa62b678dcdfdf7934ddd6e127e
             {
                 salt: ENTRYPOINT_SALT_V07,
                 bytecode: EntryPoint.bytecode,
-            });
+            },
+        );
         if (entryPointDeploy.hash) {
             await client.waitForTransactionReceipt({ hash: entryPointDeploy.hash });
         }
     }
 
     // Forge script
-    const templateCommand = `forge script ./script/DeployLocal.s.sol --private-key ${anvil0} --broadcast`;
+    const templateCommand = `forge script ./script/DeployLocal.s.sol --private-key ${anvil0} --broadcast --code-size-limit 693216`;
     const { stdout } = await execPromise(templateCommand);
 
     // Topup Paymasters
@@ -95,8 +110,8 @@ export async function setup() {
             address: contracts.openPaymaster,
             abi: OpenPaymaster.abi,
             functionName: "deposit",
-            value: parseEther("10")
-        })
+            value: parseEther("10"),
+        });
         await client.waitForTransactionReceipt({ hash: depositOpenPaymasterHash });
 
         // Deposit BalanceDeltaPaymaster
@@ -104,12 +119,12 @@ export async function setup() {
             address: contracts.balanceDeltaPaymaster,
             abi: BalanceDeltaPaymaster.abi,
             functionName: "deposit",
-            value: parseEther("10")
-        })
+            value: parseEther("10"),
+        });
         await client.waitForTransactionReceipt({ hash: depositBalanceDeltaPaymasterHash });
     }
 
-    const executorPrivateKeys: Hex[] = [anvil1]
+    const executorPrivateKeys: Hex[] = [anvil1];
     bundlerL1Instance = alto({
         safeMode: false, // avoid opcode checks
         port: opChainL1BundlerPort,
@@ -117,23 +132,23 @@ export async function setup() {
         rpcUrl: opChainL1.rpcUrls.default.http[0],
         executorPrivateKeys,
         utilityPrivateKey: anvil2,
-    })
+    });
     bundlerAInstance = alto({
         safeMode: false, // avoid opcode checks
         port: opChainABundlerPort,
         entrypoints: [entryPoint07Address],
         rpcUrl: opChainA.rpcUrls.default.http[0],
         executorPrivateKeys,
-        utilityPrivateKey: anvil2
-    })
+        utilityPrivateKey: anvil2,
+    });
     bundlerBInstance = alto({
         safeMode: false, // avoid opcode checks
         port: opChainBBundlerPort,
         entrypoints: [entryPoint07Address],
         rpcUrl: opChainB.rpcUrls.default.http[0],
         executorPrivateKeys,
-        utilityPrivateKey: anvil2
-    })
+        utilityPrivateKey: anvil2,
+    });
 
     await bundlerL1Instance.start();
     await bundlerAInstance.start();
