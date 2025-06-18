7825ba5ec8a6cf7c9281c77cd2c3ace9e068b02a
Leo Vigna <leo.vigna@gmail.com>
2025-06-18 17:32:44 +0200

    [WIP] bridge swap tx

--- packages/veraswap-sdk/src/uniswap/quote/getRouteTransaction.ts
+++ packages/veraswap-sdk/src/uniswap/quote/getRouteTransaction.ts
@@ -1,20 +1,27 @@
 import { QueryClient } from "@tanstack/react-query";
 import { Config } from "@wagmi/core";
-import { readContractQueryOptions } from "@wagmi/core/query";
-import { Address, encodeFunctionData, Hex } from "viem";
+import { Address, encodeFunctionData, Hex, zeroHash } from "viem";
 
-import { GasRouter } from "../../artifacts/GasRouter.js";
 import { IUniversalRouter } from "../../artifacts/IUniversalRouter.js";
+import {
+    getBridgeSwapWithKernelCalls,
+    GetBridgeSwapWithKernelCallsParams,
+} from "../../calls/getBridgeSwapWithKernelCalls.js";
+import {
+    getTransferRemoteWithKernelCalls,
+    GetTransferRemoteWithKernelCallsParams,
+} from "../../calls/getTransferRemoteWithKernelCalls.js";
 import {
     getPermit2PermitSignature,
     GetPermit2PermitSignatureParams,
 } from "../../calls/Permit2/getPermit2PermitSignature.js";
 import { MAX_UINT_160 } from "../../constants/uint256.js";
-import { Currency, getUniswapV4Address } from "../../currency/currency.js";
+import { Currency, getUniswapV4Address, isMultichainToken } from "../../currency/currency.js";
 import { getSuperchainBridgeTransaction } from "../../superchain/getSuperchainBridgeTransaction.js";
 import { getTransferRemoteCall } from "../../swap/getTransferRemoteCall.js";
 import { PermitSingle } from "../../types/AllowanceTransfer.js";
 import { PoolKeyOptions } from "../../types/PoolKey.js";
+import { TokenStandard } from "../../types/Token.js";
 import { getHyperlaneTransferRemoteCommand } from "../commands/getHyperlaneTransferRemoteCommand.js";
 import { getOrbiterBridgeAllETHCommand } from "../commands/getOrbiterBridgeAllEthCommand.js";
 import { getSuperchainSendAllERC20Command } from "../commands/getSuperchainSendAllERC20Command.js";
@@ -26,12 +33,28 @@ import { getRouterCommandsForQuote } from "./getUniswapRoute.js";
 //TODO: Add slippage?
 export interface GetRouteTransactionParams {
     account: Address;
+    smartAccount: {
+        initData: Hex;
+    };
     currencyIn: Currency;
     currencyOut: Currency;
     amountIn: bigint;
     recipient?: Address;
     currencyHopsByChain: Record<number, Address[] | undefined>;
-    contractsByChain: Record<number, { universalRouter: Address; metaQuoter: Address; weth9: Address } | undefined>;
+    contractsByChain: Record<
+        number,
+        | {
+            execute: Address;
+            kernelFactory: Address;
+            ownableSignatureExecutor: Address;
+            erc7579Router: Address;
+            universalRouter: Address;
+            metaQuoter: Address;
+            weth9: Address;
+            interchainGasPaymaster: Address;
+        }
+        | undefined
+    >;
     poolKeyOptions?: PoolKeyOptions[];
 }
 
@@ -39,8 +62,8 @@ export async function getRouteTransaction(
     queryClient: QueryClient,
     wagmiConfig: Config,
     params: GetRouteTransactionParams,
-): Promise<{ to: Address; data: Hex; value: bigint } | null> {
-    const { account, contractsByChain } = params;
+): Promise<{ to: Address; data?: Hex; value?: bigint } | null> {
+    const { account, smartAccount, contractsByChain } = params;
 
     const steps = await getRouteSteps(queryClient, wagmiConfig, params);
     if (!steps) return null;
@@ -124,22 +147,32 @@ export async function getRouteTransaction(
                 destination: currencyOut.chainId,
             });
         } else if (step.type === "hyperlane") {
-            const { currencyIn, currencyOut, amountIn, recipient } = step;
-            const router = currencyIn.hyperlaneAddress!;
-            const bridgePayment = await queryClient.fetchQuery(
-                readContractQueryOptions(wagmiConfig, {
-                    chainId: currencyIn.chainId,
-                    address: router,
-                    abi: GasRouter.abi,
-                    functionName: "quoteGasPayment",
-                    args: [currencyOut.chainId],
-                }),
-            );
+            const { currencyIn, currencyOut, amountIn, recipient, bridgePayment } = step;
 
             if (currencyIn.hyperlaneAddress != currencyIn.address) {
+                const contracts = contractsByChain[step.currencyIn.chainId];
+                if (!contracts) return null; // No contracts available for this chain
                 // HypERC20Collateral (collateral has different address)
                 // TODO: Implement transfer via smart account?
-                return null;
+                const bridgeParams: GetTransferRemoteWithKernelCallsParams = {
+                    chainId: currencyIn.chainId,
+                    token: currencyIn.hyperlaneAddress!,
+                    tokenStandard: "HypERC20Collateral",
+                    account,
+                    destination: currencyOut.chainId,
+                    recipient: recipient ?? account,
+                    amount: amountIn,
+                    //TODO: LOCAL CONTRACTS
+                    createAccount: {
+                        initData: smartAccount.initData,
+                        salt: zeroHash,
+                        factoryAddress: contracts.kernelFactory,
+                    },
+                    contracts,
+                };
+
+                const result = await getTransferRemoteWithKernelCalls(queryClient, wagmiConfig, bridgeParams);
+                return result.calls[0];
             } else {
                 // HypERC20 (same address)
                 // TODO: Need to quote gas
@@ -223,7 +256,7 @@ export async function getRouteTransaction(
             } else if (bridge.type === "hyperlane") {
                 // Hyperlane bridge
                 const bridgeCommand = getHyperlaneTransferRemoteCommand({
-                    bridgePayment: 0n, // TODO: Add bridge payment if needed
+                    bridgePayment: bridge.bridgePayment,
                     router: bridge.currencyIn.hyperlaneAddress!,
                     destination: bridge.currencyOut.chainId,
                     recipient: bridge.recipient ?? account,
@@ -244,9 +277,58 @@ export async function getRouteTransaction(
             };
         } else if (steps[0].type != "uniswap" && steps[1].type === "uniswap") {
             // Bridge & Swap
-            // const [bridge, swap] = steps;
-            //TODO: Implement this case
-            return null;
+            const [bridge, swap] = steps;
+            const contracts = contractsByChain[bridge.currencyIn.chainId];
+            if (!contracts) return null; // No contracts available for this chain
+            const contractsRemote = contractsByChain[bridge.currencyOut.chainId];
+            if (!contractsRemote) return null; // No contracts available for this chain
+
+            const bridgeSwapParams: GetBridgeSwapWithKernelCallsParams = {
+                chainId: bridge.currencyIn.chainId,
+                token: isMultichainToken(bridge.currencyIn)
+                    ? (bridge.currencyIn.hyperlaneAddress ?? bridge.currencyIn.address)
+                    : getUniswapV4Address(bridge.currencyIn),
+                tokenStandard: getTokenStandard(bridge.currencyIn),
+                tokenOutStandard: getTokenStandard(bridge.currencyOut),
+                account: account,
+                destination: bridge.currencyOut.chainId,
+                recipient: account ?? bridge.recipient,
+                amount: bridge.amountIn,
+                //TODO: LOCAL CONTRACTS
+                contracts,
+                contractsRemote,
+                createAccount: {
+                    initData: smartAccount.initData,
+                    salt: zeroHash,
+                    factoryAddress: contracts.kernelFactory,
+                },
+                createAccountRemote: {
+                    initData: smartAccount.initData,
+                    salt: zeroHash,
+                    factoryAddress: contracts.kernelFactory,
+                },
+                // erc7579RouterOwners: [],
+                // erc7579RouterOwnersRemote: [],
+                remoteSwapParams: {
+                    // Adjust amount in if using orbiter to account for fees
+                    amountIn: remoteSwapAmountIn,
+                    amountOutMinimum,
+                    path,
+                    currencyIn: getUniswapV4Address(swapCurrencyIn),
+                    currencyOut: getUniswapV4Address(swapCurrencyOut),
+                    receiver: walletAddress,
+                    universalRouter: contracts[currencyOut.chainId].universalRouter,
+                },
+                orbiterQuote,
+            };
+
+            const result = await getBridgeSwapWithKernelCalls(queryClient, wagmiConfig, bridgeSwapParams);
+
+            return result.calls[0] as {
+                to: Address;
+                data: Hex;
+                value: bigint;
+            };
         } else {
             return null; // Invalid route, must have one swap and one bridge
         }
@@ -254,4 +336,15 @@ export async function getRouteTransaction(
         // Unsupported 3 steps route
         return null;
     }
+}
+
+// TODO: Temp function to match old TokenStandard
+function getTokenStandard(currency: Currency): TokenStandard {
+    if (currency.isNative) return "NativeToken";
+    if (isMultichainToken(currency)) {
+        if (currency.isSuperERC20()) return "SuperchainERC20";
+        if (currency.isHypERC20()) return "HypERC20";
+        if (currency.hyperlaneAddress) return "HypERC20Collateral";
+    }
+    return "ERC20";
 }
