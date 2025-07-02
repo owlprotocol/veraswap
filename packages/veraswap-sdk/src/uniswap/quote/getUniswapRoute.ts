55f636a4c2e3602d068383049691df2b00855fc7
Oscar Baracos <oscar.baracos@gmail.com>
2025-07-02 17:29:30 +0200

    Refactor and fix router command encoding

--- packages/veraswap-sdk/src/uniswap/quote/getUniswapRoute.ts
+++ packages/veraswap-sdk/src/uniswap/quote/getUniswapRoute.ts
@@ -27,7 +27,7 @@ export interface GetUniswapRouteParams {
 }
 
 // TODO: Convert to queryOptions helper (only single query needed)
-// TODO: Implement more generic swap encoding logi
+// TODO: Implement more generic swap encoding logic
 // TODO: Add more invariants, refactor as smaller functions
 export async function getUniswapRouteExactIn(
     queryClient: QueryClient,
@@ -118,6 +118,260 @@ export function getRouterCommandsForQuote(params: GetRouterCommandsForQuote): Cr
     }
 }
 
+const isEthOrWeth = (currency: Address, weth9: Address) => currency === zeroAddress || currency === weth9;
+
+type Path = MetaQuoteBestMultihop["path"];
+type PathHop = Path[number];
+
+function getMultihopV3ToV3Commands({
+    currencyIn,
+    amountIn,
+    quoteVariableAmount,
+    hop0,
+    hop1,
+    recipient = ACTION_CONSTANTS.MSG_SENDER,
+    weth9,
+}: {
+    currencyIn: Address;
+    amountIn: bigint;
+    quoteVariableAmount: bigint;
+    hop0: PathHop;
+    hop1: PathHop;
+    recipient?: Address;
+    weth9: Address;
+}) {
+    const commands: CreateCommandParamsGeneric[] = [];
+
+    // Wrap ETH to WETH if needed
+    const wrapNative = currencyIn === zeroAddress;
+    if (wrapNative) {
+        commands.push([CommandType.WRAP_ETH, [ACTION_CONSTANTS.ADDRESS_THIS, ACTION_CONSTANTS.CONTRACT_BALANCE]]);
+    }
+
+    // V3 Trade Plan
+    const v3SwapCurrencyIn = currencyIn === zeroAddress ? weth9 : currencyIn;
+    const v3SwapCurrencyIntermediate = hop0.intermediateCurrency === zeroAddress ? weth9 : hop0.intermediateCurrency;
+    const v3SwapCurrencyOutput = hop1.intermediateCurrency === zeroAddress ? weth9 : hop1.intermediateCurrency;
+
+    const v3Path = encodePacked(
+        ["address", "uint24", "address", "uint24", "address"],
+        [v3SwapCurrencyIn, hop0.fee, v3SwapCurrencyIntermediate, hop1.fee, v3SwapCurrencyOutput],
+    );
+
+    // TODO: Assumes output is never WETH, which is not always true
+    const unwrapWeth = isEthOrWeth(hop1.intermediateCurrency, weth9);
+    const v3TradePlan: [Address, bigint, bigint, Hex, boolean] = [
+        unwrapWeth ? ACTION_CONSTANTS.ADDRESS_THIS : recipient, // WETH received to router
+        amountIn, // exact amount in
+        quoteVariableAmount,
+        v3Path,
+        !wrapNative, // payerIsUser (WETH received to router)
+    ];
+    commands.push([CommandType.V3_SWAP_EXACT_IN, v3TradePlan]);
+
+    // Unwrap WETH to ETH if needed
+    if (unwrapWeth) {
+        commands.push([CommandType.UNWRAP_WETH, [recipient, quoteVariableAmount]]);
+    }
+
+    return commands;
+}
+
+function getMultihopV4ToV4Commands({
+    currencyIn,
+    amountIn,
+    quoteVariableAmount,
+    path,
+    recipient,
+}: {
+    currencyIn: Address;
+    amountIn: bigint;
+    quoteVariableAmount: bigint;
+    path: Path;
+    recipient?: Address;
+}) {
+    const commands: CreateCommandParamsGeneric[] = [];
+    const [, hop1] = path;
+
+    const v4TradePlan = new V4Planner();
+    v4TradePlan.addAction(Actions.SWAP_EXACT_IN, [
+        {
+            currencyIn,
+            path,
+            amountIn,
+            amountOutMinimum: quoteVariableAmount,
+        },
+    ]);
+    v4TradePlan.addAction(Actions.SETTLE_ALL, [currencyIn, amountIn]);
+    if (!recipient) {
+        v4TradePlan.addAction(Actions.TAKE_ALL, [hop1.intermediateCurrency, quoteVariableAmount]);
+    } else {
+        v4TradePlan.addAction(Actions.TAKE, [hop1.intermediateCurrency, recipient, quoteVariableAmount]);
+    }
+    commands.push([CommandType.V4_SWAP, [v4TradePlan.finalize() as Hex]]);
+
+    return commands;
+}
+
+function getMultihopV3ToV4Commands({
+    currencyIn,
+    amountIn,
+    quoteVariableAmount,
+    hop0,
+    hop1,
+    recipient,
+    weth9,
+}: {
+    currencyIn: Address;
+    amountIn: bigint;
+    quoteVariableAmount: bigint;
+    hop0: PathHop;
+    hop1: PathHop;
+    recipient?: Address;
+    weth9: Address;
+}) {
+    const commands: CreateCommandParamsGeneric[] = [];
+
+    // Wrap ETH to WETH if needed
+    const wrapNative = currencyIn === zeroAddress;
+    if (wrapNative) {
+        commands.push([CommandType.WRAP_ETH, [ACTION_CONSTANTS.ADDRESS_THIS, ACTION_CONSTANTS.CONTRACT_BALANCE]]);
+    }
+
+    // V3 Trade Plan
+    const v3SwapCurrencyIn = wrapNative ? weth9 : currencyIn;
+    const v3SwapCurrencyIntermediate = hop0.intermediateCurrency === zeroAddress ? weth9 : hop0.intermediateCurrency;
+    const v3Path = encodePacked(
+        ["address", "uint24", "address"],
+        [v3SwapCurrencyIn, hop0.fee, v3SwapCurrencyIntermediate],
+    );
+    const v3TradePlan: [Address, bigint, bigint, Hex, boolean] = [
+        ACTION_CONSTANTS.ADDRESS_THIS, // recipient
+        amountIn, // exact amount in
+        0n, // amountOutMinimum ignored for intermediate swap
+        v3Path,
+        !wrapNative, // payerIsUser (WETH received to router)
+    ];
+    commands.push([CommandType.V3_SWAP_EXACT_IN, v3TradePlan]);
+
+    // Unwrap WETH to ETH if needed
+    const unwrapWeth = isEthOrWeth(hop0.intermediateCurrency, weth9);
+    if (unwrapWeth) {
+        commands.push([CommandType.UNWRAP_WETH, [ACTION_CONSTANTS.ADDRESS_THIS, 0n]]);
+    }
+
+    // V4 Trade Plan
+    const v4SwapCurrencyIn = unwrapWeth ? zeroAddress : hop0.intermediateCurrency;
+    const [v4Currency0, v4Currency1] =
+        v4SwapCurrencyIn < hop1.intermediateCurrency
+            ? [v4SwapCurrencyIn, hop1.intermediateCurrency]
+            : [hop1.intermediateCurrency, v4SwapCurrencyIn];
+    const v4TradePlan = new V4Planner();
+    v4TradePlan.addAction(Actions.SETTLE, [v4SwapCurrencyIn, ACTION_CONSTANTS.CONTRACT_BALANCE, false]); // Open delta for intermediateCurrency
+    v4TradePlan.addAction(Actions.SWAP_EXACT_IN_SINGLE, [
+        {
+            poolKey: {
+                currency0: v4Currency0,
+                currency1: v4Currency1,
+                fee: hop1.fee,
+                tickSpacing: hop1.tickSpacing,
+                hooks: hop1.hooks,
+            },
+            zeroForOne: v4Currency0 == v4SwapCurrencyIn,
+            amountIn: ACTION_CONSTANTS.OPEN_DELTA,
+            amountOutMinimum: quoteVariableAmount,
+            hookData: hop1.hookData,
+        },
+    ]);
+    if (!recipient) {
+        v4TradePlan.addAction(Actions.TAKE_ALL, [hop1.intermediateCurrency, quoteVariableAmount]);
+    } else {
+        v4TradePlan.addAction(Actions.TAKE, [hop1.intermediateCurrency, recipient, quoteVariableAmount]);
+    }
+    commands.push([CommandType.V4_SWAP, [v4TradePlan.finalize() as Hex]]);
+
+    return commands;
+}
+
+// Mixed V4 -> V3 multihop commands
+function getMultihopV4ToV3Commands({
+    currencyIn,
+    amountIn,
+    quoteVariableAmount,
+    hop0,
+    hop1,
+    recipient = ACTION_CONSTANTS.MSG_SENDER,
+    weth9,
+}: {
+    currencyIn: Address;
+    amountIn: bigint;
+    quoteVariableAmount: bigint;
+    hop0: PathHop;
+    hop1: PathHop;
+    recipient?: Address;
+    weth9: Address;
+}) {
+    const commands: CreateCommandParamsGeneric[] = [];
+
+    // Uniswap V4 pool
+    const v4TradePlan = new V4Planner();
+    const [v4Currency0, v4Currency1] =
+        currencyIn < hop0.intermediateCurrency
+            ? [currencyIn, hop0.intermediateCurrency]
+            : [hop0.intermediateCurrency, currencyIn];
+    v4TradePlan.addAction(Actions.SWAP_EXACT_IN_SINGLE, [
+        {
+            poolKey: {
+                currency0: v4Currency0,
+                currency1: v4Currency1,
+                fee: hop0.fee,
+                tickSpacing: hop0.tickSpacing,
+                hooks: hop0.hooks,
+            },
+            zeroForOne: v4Currency0 == currencyIn,
+            amountIn,
+            amountOutMinimum: 0n, // amountOutMinimum ignored for intermediate swap
+            hookData: hop0.hookData,
+        },
+    ]);
+    v4TradePlan.addAction(Actions.SETTLE_ALL, [currencyIn, amountIn]);
+    v4TradePlan.addAction(Actions.TAKE, [
+        hop0.intermediateCurrency,
+        ACTION_CONSTANTS.ADDRESS_THIS,
+        ACTION_CONSTANTS.OPEN_DELTA,
+    ]);
+    commands.push([CommandType.V4_SWAP, [v4TradePlan.finalize() as Hex]]);
+
+    // Wrap ETH to WETH if needed
+    const wrapNative = hop0.intermediateCurrency === zeroAddress;
+    if (wrapNative) {
+        commands.push([CommandType.WRAP_ETH, [ACTION_CONSTANTS.ADDRESS_THIS, ACTION_CONSTANTS.CONTRACT_BALANCE]]);
+    }
+
+    // V3 Trade Plan
+    const v3SwapCurrencyIn = wrapNative ? weth9 : hop0.intermediateCurrency;
+    const v3SwapCurrencyOutput = hop1.intermediateCurrency === zeroAddress ? weth9 : hop1.intermediateCurrency;
+    const v3Path = encodePacked(["address", "uint24", "address"], [v3SwapCurrencyIn, hop1.fee, v3SwapCurrencyOutput]);
+
+    // TODO: Assumes output is never WETH, which is not always true
+    const unwrapWeth = isEthOrWeth(hop1.intermediateCurrency, weth9);
+    const v3TradePlan: [Address, bigint, bigint, Hex, boolean] = [
+        unwrapWeth ? ACTION_CONSTANTS.ADDRESS_THIS : recipient, // WETH received to router
+        ACTION_CONSTANTS.CONTRACT_BALANCE,
+        quoteVariableAmount,
+        v3Path,
+        false, // payerIsUser
+    ];
+    commands.push([CommandType.V3_SWAP_EXACT_IN, v3TradePlan]);
+
+    // Unwrap WETH to ETH if needed
+    if (unwrapWeth) {
+        commands.push([CommandType.UNWRAP_WETH, [recipient, quoteVariableAmount]]);
+    }
+
+    return commands;
+}
+
 export interface GetRouterCommandsForMultihopQuoteParams {
     currencyIn: Address;
     amountIn: bigint;
@@ -140,176 +394,47 @@ export function getRouterCommandsForMultihopQuote(
     const [hop0, hop1] = quote.path;
 
     if (hop0.hooks === address3 && hop1.hooks === address3) {
-        // Multihop V3 -> V3
-        // Wrap ETH to WETH if needed
-        const wrapNative = currencyIn === zeroAddress;
-        if (wrapNative) {
-            commands.push([CommandType.WRAP_ETH, [ACTION_CONSTANTS.ADDRESS_THIS, ACTION_CONSTANTS.CONTRACT_BALANCE]]);
-        }
-
-        // V3 Trade Plan
-        const v3SwapCurrencyIn = currencyIn === zeroAddress ? weth9 : currencyIn;
-        const v3SwapCurrencyIntermediate =
-            hop0.intermediateCurrency === zeroAddress ? weth9 : hop0.intermediateCurrency;
-        const v3SwapCurrencyOutput = hop1.intermediateCurrency === zeroAddress ? weth9 : hop1.intermediateCurrency;
-
-        const v3Path = encodePacked(
-            ["address", "uint24", "address", "uint24", "address"],
-            [v3SwapCurrencyIn, hop0.fee, v3SwapCurrencyIntermediate, hop1.fee, v3SwapCurrencyOutput],
-        );
-        const unwrapWeth = hop1.intermediateCurrency === weth9;
-        const recipient = params.recipient ?? ACTION_CONSTANTS.MSG_SENDER;
-        const v3TradePlan: [Address, bigint, bigint, Hex, boolean] = [
-            unwrapWeth ? ACTION_CONSTANTS.ADDRESS_THIS : recipient, // WETH received to router
-            amountIn, // exact amount in
-            quote.variableAmount,
-            v3Path,
-            !wrapNative, // payerIsUser (WETH received to router)
-        ];
-        commands.push([CommandType.V3_SWAP_EXACT_IN, v3TradePlan]);
-
-        // Unwrap WETH to ETH if needed
-        if (unwrapWeth) {
-            commands.push([CommandType.UNWRAP_WETH, [recipient, quote.variableAmount]]);
-        }
+        const v3ToV3Commands = getMultihopV3ToV3Commands({
+            amountIn,
+            currencyIn,
+            hop0,
+            hop1,
+            quoteVariableAmount: quote.variableAmount,
+            recipient: params.recipient,
+            weth9,
+        });
+        commands.push(...v3ToV3Commands);
     } else if (hop0.hooks !== address3 && hop1.hooks !== address3) {
-        // Multihop V4 -> V4
-        const v4TradePlan = new V4Planner();
-        v4TradePlan.addAction(Actions.SWAP_EXACT_IN, [
-            {
-                currencyIn,
-                path: quote.path,
-                amountIn,
-                amountOutMinimum: quote.variableAmount,
-            },
-        ]);
-        v4TradePlan.addAction(Actions.SETTLE_ALL, [currencyIn, amountIn]);
-        if (!params.recipient) {
-            v4TradePlan.addAction(Actions.TAKE_ALL, [hop1.intermediateCurrency, quote.variableAmount]);
-        } else {
-            v4TradePlan.addAction(Actions.TAKE, [hop1.intermediateCurrency, params.recipient, quote.variableAmount]);
-        }
-        commands.push([CommandType.V4_SWAP, [v4TradePlan.finalize() as Hex]]);
+        const v4ToV4Commands = getMultihopV4ToV4Commands({
+            currencyIn,
+            amountIn,
+            quoteVariableAmount: quote.variableAmount,
+            path: quote.path,
+            recipient: params.recipient,
+        });
+        commands.push(...v4ToV4Commands);
     } else if (hop0.hooks === address3 && hop1.hooks !== address3) {
-        // Mixed Route V3 -> V4
-        // Wrap ETH to WETH if needed
-        const wrapNative = currencyIn === zeroAddress;
-        if (wrapNative) {
-            commands.push([CommandType.WRAP_ETH, [ACTION_CONSTANTS.ADDRESS_THIS, ACTION_CONSTANTS.CONTRACT_BALANCE]]);
-        }
-
-        // V3 Trade Plan
-        const v3SwapCurrencyIn = wrapNative ? weth9 : currencyIn;
-        const v3SwapCurrencyIntermediate =
-            hop0.intermediateCurrency === zeroAddress ? weth9 : hop0.intermediateCurrency;
-        const v3Path = encodePacked(
-            ["address", "uint24", "address"],
-            [v3SwapCurrencyIn, hop0.fee, v3SwapCurrencyIntermediate],
-        );
-        const v3TradePlan: [Address, bigint, bigint, Hex, boolean] = [
-            ACTION_CONSTANTS.ADDRESS_THIS, // recipient
-            amountIn, // exact amount in
-            0n, // amountOutMinimum ignored for intermediate swap
-            v3Path,
-            !wrapNative, // payerIsUser (WETH received to router)
-        ];
-        commands.push([CommandType.V3_SWAP_EXACT_IN, v3TradePlan]);
-
-        // Unwrap WETH to ETH if needed
-        // TODO: check, is intermediateCurrency not zeroAddress?
-        const unwrapWeth = hop0.intermediateCurrency === weth9;
-        if (unwrapWeth) {
-            commands.push([CommandType.UNWRAP_WETH, [ACTION_CONSTANTS.ADDRESS_THIS, 0n]]);
-        }
-
-        // V4 Trade Plan
-        const v4SwapCurrencyIn = unwrapWeth ? zeroAddress : hop0.intermediateCurrency;
-        const [v4Currency0, v4Currency1] =
-            v4SwapCurrencyIn < hop1.intermediateCurrency
-                ? [v4SwapCurrencyIn, hop1.intermediateCurrency]
-                : [hop1.intermediateCurrency, v4SwapCurrencyIn];
-        const v4TradePlan = new V4Planner();
-        v4TradePlan.addAction(Actions.SETTLE, [v4SwapCurrencyIn, ACTION_CONSTANTS.CONTRACT_BALANCE, false]); // Open delta for intermediateCurrency
-        v4TradePlan.addAction(Actions.SWAP_EXACT_IN_SINGLE, [
-            {
-                poolKey: {
-                    currency0: v4Currency0,
-                    currency1: v4Currency1,
-                    fee: hop1.fee,
-                    tickSpacing: hop1.tickSpacing,
-                    hooks: hop1.hooks,
-                },
-                zeroForOne: v4Currency0 == v4SwapCurrencyIn,
-                amountIn: ACTION_CONSTANTS.OPEN_DELTA,
-                amountOutMinimum: quote.variableAmount,
-                hookData: hop1.hookData,
-            },
-        ]);
-        if (!params.recipient) {
-            v4TradePlan.addAction(Actions.TAKE_ALL, [hop1.intermediateCurrency, quote.variableAmount]);
-        } else {
-            v4TradePlan.addAction(Actions.TAKE, [hop1.intermediateCurrency, params.recipient, quote.variableAmount]);
-        }
-        commands.push([CommandType.V4_SWAP, [v4TradePlan.finalize() as Hex]]);
+        const v3ToV4Commands = getMultihopV3ToV4Commands({
+            amountIn,
+            currencyIn,
+            hop0,
+            hop1,
+            quoteVariableAmount: quote.variableAmount,
+            recipient: params.recipient,
+            weth9,
+        });
+        commands.push(...v3ToV4Commands);
     } else if (hop0.hooks !== address3 && hop1.hooks === address3) {
-        // Mixed Route V4 -> V3
-        // Uniswap V4 pool
-        const v4TradePlan = new V4Planner();
-        const [v4Currency0, v4Currency1] =
-            currencyIn < hop0.intermediateCurrency
-                ? [currencyIn, hop0.intermediateCurrency]
-                : [hop0.intermediateCurrency, currencyIn];
-        v4TradePlan.addAction(Actions.SWAP_EXACT_IN_SINGLE, [
-            {
-                poolKey: {
-                    currency0: v4Currency0,
-                    currency1: v4Currency1,
-                    fee: hop0.fee,
-                    tickSpacing: hop0.tickSpacing,
-                    hooks: hop0.hooks,
-                },
-                zeroForOne: v4Currency0 == currencyIn,
-                amountIn,
-                amountOutMinimum: 0n, // amountOutMinimum ignored for intermediate swap
-                hookData: hop0.hookData,
-            },
-        ]);
-        v4TradePlan.addAction(Actions.SETTLE_ALL, [currencyIn, amountIn]);
-        v4TradePlan.addAction(Actions.TAKE, [
-            hop0.intermediateCurrency,
-            ACTION_CONSTANTS.ADDRESS_THIS,
-            ACTION_CONSTANTS.OPEN_DELTA,
-        ]);
-        commands.push([CommandType.V4_SWAP, [v4TradePlan.finalize() as Hex]]);
-
-        // Wrap ETH to WETH if needed
-        const wrapNative = hop0.intermediateCurrency === zeroAddress;
-        if (wrapNative) {
-            commands.push([CommandType.WRAP_ETH, [ACTION_CONSTANTS.ADDRESS_THIS, ACTION_CONSTANTS.CONTRACT_BALANCE]]);
-        }
-
-        // V3 Trade Plan
-        const v3SwapCurrencyIn = wrapNative ? weth9 : hop0.intermediateCurrency;
-        const v3SwapCurrencyOutput = hop1.intermediateCurrency === zeroAddress ? weth9 : hop1.intermediateCurrency;
-        const v3Path = encodePacked(
-            ["address", "uint24", "address"],
-            [v3SwapCurrencyIn, hop1.fee, v3SwapCurrencyOutput],
-        );
-        const unwrapWeth = hop1.intermediateCurrency === weth9;
-        const recipient = params.recipient ?? ACTION_CONSTANTS.MSG_SENDER;
-        const v3TradePlan: [Address, bigint, bigint, Hex, boolean] = [
-            unwrapWeth ? ACTION_CONSTANTS.ADDRESS_THIS : recipient, // WETH received to router
-            ACTION_CONSTANTS.CONTRACT_BALANCE,
-            quote.variableAmount,
-            v3Path,
-            false, // payerIsUser
-        ];
-        commands.push([CommandType.V3_SWAP_EXACT_IN, v3TradePlan]);
-
-        // Unwrap WETH to ETH if needed
-        if (unwrapWeth) {
-            commands.push([CommandType.UNWRAP_WETH, [recipient, quote.variableAmount]]);
-        }
+        const v4ToV3Commands = getMultihopV4ToV3Commands({
+            amountIn,
+            currencyIn,
+            hop0,
+            hop1,
+            quoteVariableAmount: quote.variableAmount,
+            recipient: params.recipient,
+            weth9,
+        });
+        commands.push(...v4ToV3Commands);
     }
 
     return commands;
