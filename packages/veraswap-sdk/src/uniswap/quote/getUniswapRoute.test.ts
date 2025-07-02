51cc70edb25fc86c253f2b9f6ad7956ed28507a0
Oscar Baracos <oscar.baracos@gmail.com>
2025-07-02 13:45:49 +0200

    [WIP] add uniswap route test for v4 -> eth -> v3 multihop

--- packages/veraswap-sdk/src/uniswap/quote/getUniswapRoute.test.ts
+++ packages/veraswap-sdk/src/uniswap/quote/getUniswapRoute.test.ts
@@ -5,7 +5,7 @@ import { describe, expect, test } from "vitest";
 import { IERC20 } from "../../artifacts/IERC20.js";
 import { IUniversalRouter } from "../../artifacts/IUniversalRouter.js";
 import { opChainL1, opChainL1Client } from "../../chains/supersim.js";
-import { LOCAL_CURRENCIES } from "../../constants/tokens.js";
+import { getMockERC20Address, LOCAL_CURRENCIES } from "../../constants/tokens.js";
 import { LOCAL_UNISWAP_CONTRACTS } from "../../constants/uniswap.js";
 import { getUniswapV4Address } from "../../currency/currency.js";
 import { anvilClientL1, wagmiConfig } from "../../test/constants.js";
@@ -24,6 +24,7 @@ describe("uniswap/quote/getUniswapRoute.test.ts", function () {
         "error CurrencyNotSettled()",
         "error PoolNotInitialized()",
         "error V3TooLittleReceived()",
+        "error SwapAmountCannotBeZero()",
     ]);
 
     const amountIn = parseUnits("0.01", 18);
@@ -35,6 +36,7 @@ describe("uniswap/quote/getUniswapRoute.test.ts", function () {
     const tokenL34 = getUniswapV4Address(LOCAL_CURRENCIES[6]);
     const tokenL3 = getUniswapV4Address(LOCAL_CURRENCIES[7]);
     const tokenL4 = getUniswapV4Address(LOCAL_CURRENCIES[8]);
+    const tokenZ = getMockERC20Address({ name: "Token Z", symbol: "Z", decimals: 18 });
 
     // Helper function to get balance of a token
     const getBalance = function (token: Address) {
@@ -291,6 +293,51 @@ describe("uniswap/quote/getUniswapRoute.test.ts", function () {
             const currencyIn = tokenL3;
             const currencyOut = tokenL4;
             const hopCurrencies = [tokenL34];
+
+            const contracts = {
+                weth9: LOCAL_UNISWAP_CONTRACTS.weth9,
+                metaQuoter: LOCAL_UNISWAP_CONTRACTS.metaQuoter,
+            };
+
+            // Route
+            const route = await getUniswapRouteExactIn(queryClient, wagmiConfig, {
+                chainId: opChainL1.id,
+                currencyIn,
+                currencyOut,
+                currencyHops: hopCurrencies,
+                amountIn,
+                contracts,
+            });
+            expect(route).toBeDefined();
+            const { quote, amountOut, value } = route!;
+
+            const commands = getRouterCommandsForQuote({ currencyIn, currencyOut, amountIn, contracts, ...quote });
+
+            const routePlanner = new RoutePlanner();
+            addCommandsToRoutePlanner(routePlanner, commands);
+
+            //Execute
+            const currencyInBalanceBeforeSwap = await getBalance(currencyIn);
+            const currencyOutBalanceBeforeSwap = await getBalance(currencyOut);
+            const deadline = BigInt(Math.floor(Date.now() / 1000) + 3600);
+            const hash = await anvilClientL1.writeContract({
+                abi: [...IUniversalRouter.abi, ...UniswapErrorAbi],
+                address: LOCAL_UNISWAP_CONTRACTS.universalRouter,
+                value,
+                functionName: "execute",
+                args: [routePlanner.commands, routePlanner.inputs, deadline],
+            });
+            await opChainL1Client.waitForTransactionReceipt({ hash });
+            const currencyInBalanceAfterSwap = await getBalance(currencyIn);
+            const currencyOutBalanceAfterSwap = await getBalance(currencyOut);
+            expect(currencyInBalanceAfterSwap).toBe(currencyInBalanceBeforeSwap - amountIn); // Input balance decreased by exact amount
+            expect(currencyOutBalanceAfterSwap).toBe(currencyOutBalanceBeforeSwap + amountOut); // Output balance increased by variable amount
+        });
+
+        test("Z -> ETH -> A", async () => {
+            const currencyIn = tokenZ;
+            const currencyOut = tokenA;
+            const hopCurrencies = [zeroAddress];
 
             const contracts = {
                 weth9: LOCAL_UNISWAP_CONTRACTS.weth9,
