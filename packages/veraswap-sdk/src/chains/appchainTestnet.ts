176361a7672f76b90697da19cef9631c42c3d6e5
Oscar Baracos <oscar.baracos@gmail.com>
2025-08-07 18:23:33 -0600

    Add chain definitions

---
+++ packages/veraswap-sdk/src/chains/appchainTestnet.ts
@@ -0,0 +1,24 @@
+import { defineChain } from "viem";
+
+import { ChainWithMetadata } from "./chainWithMetadata.js";
+
+export const appchainTestnet = /* #__PURE__ */ defineChain({
+    id: 4661,
+    name: "Appchain Testnet",
+    testnet: true,
+    rpcUrls: {
+        default: {
+            http: ["https://appchaintestnet.rpc.caldera.xyz/http"],
+            webSocket: ["wss://appchaintestnet.rpc.caldera.xyz/ws"],
+        },
+    },
+    nativeCurrency: {
+        decimals: 18,
+        name: "Ether",
+        symbol: "ETH",
+    },
+    custom: {
+        logoURI: "https://raw.githubusercontent.com/hyperlane-xyz/hyperlane-registry/main/chains/appchain/logo.svg",
+    },
+    iconUrl: "https://raw.githubusercontent.com/hyperlane-xyz/hyperlane-registry/main/chains/appchain/logo.svg",
+}) satisfies ChainWithMetadata;
---
+++ packages/veraswap-sdk/src/chains/rariTestnet.ts
@@ -0,0 +1,24 @@
+import { defineChain } from "viem";
+
+import { ChainWithMetadata } from "./chainWithMetadata.js";
+
+export const rariTestnet = /* #__PURE__ */ defineChain({
+    id: 1918988905,
+    name: "Rari Testnet",
+    testnet: true,
+    rpcUrls: {
+        default: {
+            http: ["https://testnet.rpc.rarichain.org/http"],
+            webSocket: ["wss://rpc.rari.testnet"],
+        },
+    },
+    nativeCurrency: {
+        decimals: 18,
+        name: "Ether",
+        symbol: "ETH",
+    },
+    custom: {
+        logoURI: "https://raw.githubusercontent.com/hyperlane-xyz/hyperlane-registry/main/chains/rarichain/logo.svg",
+    },
+    iconUrl: "https://raw.githubusercontent.com/hyperlane-xyz/hyperlane-registry/main/chains/rarichain/logo.svg",
+}) satisfies ChainWithMetadata;
